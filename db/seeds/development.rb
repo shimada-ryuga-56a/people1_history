78eefe4af86e3f78a8a8a9bdcbb11d73c8ded943
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-01 12:07:27 +0900

    feat: seedデータのディレクトリ構成変更

---
+++ db/seeds/development.rb
@@ -0,0 +1,44 @@
+# 20.times do |n|
+#   User.create!(
+#     name: Faker::JapaneseMedia::Conan.character,
+#     email: Faker::Internet.unique.email,
+#     password: "password"
+#   )
+# end
+
+CSV.foreach('db/csv/tours.csv', headers: true) do |row|
+  Tour.create!(
+    name: row['name'],
+    name_kana_ruby: row['name_kana_ruby']
+  )
+end
+
+CSV.foreach('db/csv/events.csv', headers: true) do |row|
+  Event.create!(
+    category: row['category'],
+    name: row['name'],
+    name_kana_ruby: row['name_kana_ruby'],
+    date: row['date'],
+    place: row['place'],
+    place_prefecture: row['place_prefecture'],
+    is_canceled: row['is_canceled'],
+    remark: row['remark'],
+    tour_id: row['tour_id']
+  )
+end
+
+CSV.foreach('db/csv/songs.csv', headers: true) do |row|
+  Song.create!(
+    name: row['name'],
+    name_kana_ruby: row['name_kana_ruby'],
+    youtube_link: row['youtube']
+  )
+end
+
+# CSV.foreach('db/csv/song_informations.csv', headers: true) do |row|
+#   SongInformation.create!(
+#     user_id: row['user_id'],
+#     song_id: row['song_id'],
+#     body: row['body']
+#   )
+# end
