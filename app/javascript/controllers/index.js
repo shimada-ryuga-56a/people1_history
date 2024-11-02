0024f4e72559460860b596918f64c92fd0e673ed
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-02 13:53:17 +0900

    feat: stimulus-autocompleteインストール

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,7 @@
 import { Application } from '@hotwired/stimulus'
 import RailsNestedForm from '@stimulus-components/rails-nested-form'
+import { Autocomplete } from 'stimulus-autocomplete'
 
 const application = Application.start()
 application.register('nested-form', RailsNestedForm)
+application.register('autocomplete', Autocomplete)
