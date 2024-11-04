774478da0c73e0131dd5f27e397884bae90cecdf
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-04 12:38:29 +0900

    feat: Rubocop修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,8 +6,8 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.includes(:setlist).find(params[:id])
-    if @event.setlist
-      @setlistitems = Setlistitem.where(setlist_id: @event.setlist.id)
-    end
+    return unless @event.setlist
+
+    @setlistitems = Setlistitem.where(setlist_id: @event.setlist.id)
   end
 end
