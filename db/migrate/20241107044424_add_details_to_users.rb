f8a3df1dbddc01781b24b8eeb89ecf9cab473923
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-07 13:55:24 +0900

    add: ユーザー名・画像取得、表示

---
+++ db/migrate/20241107044424_add_details_to_users.rb
@@ -0,0 +1,5 @@
+class AddDetailsToUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_04_122410) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_07_044424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -138,6 +138,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_04_122410) do
     t.string "uid"
     t.string "provider"
     t.string "name"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
