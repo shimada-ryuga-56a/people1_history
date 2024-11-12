54c52a05809970aef5119fc4e0c30b88cd68aaf5
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-12 14:21:45 +0900

    chore: seedをfind_or_create_byに変更

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ApplicationRecord
   has_many :song_informations, dependent: :destroy
+  has_one_attached :jk
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,30 @@
 require "csv"
 
 CSV.foreach('db/csv/tours.csv', headers: true) do |row|
-  Tour.create!(
-    name: row['name'],
-    name_kana_ruby: row['name_kana_ruby']
-  )
+  Tour.find_or_create_by!(name: row['name']) do |tour|
+    tour.name = row['name']
+    tour.name_kana_ruby = row['name_kana_ruby']
+  end
 end
 
 CSV.foreach('db/csv/events.csv', headers: true) do |row|
-  Event.create!(
-    category: row['category'],
-    name: row['name'],
-    name_kana_ruby: row['name_kana_ruby'],
-    date: row['date'],
-    place: row['place'],
-    place_prefecture: row['place_prefecture'],
-    is_canceled: row['is_canceled'],
-    remark: row['remark'],
-    tour_id: row['tour_id']
-  )
+  Event.find_or_create_by!(name: row['name'], date: row['date']) do |event|
+    event.category = row['category']
+    event.name = row['name']
+    event.name_kana_ruby = row['name_kana_ruby']
+    event.date = row['date']
+    event.place = row['place']
+    event.place_prefecture = row['place_prefecture']
+    event.is_canceled = row['is_canceled']
+    event.remark = row['remark']
+    event.tour_id = row['tour_id']
+  end
 end
 
 CSV.foreach('db/csv/songs.csv', headers: true) do |row|
-  Song.create!(
-    name: row['name'],
-    name_kana_ruby: row['name_kana_ruby'],
-    youtube_link: row['youtube']
-  )
+  Song.find_or_create_by!(name: row['name']) do |song|
+    song.name = row['name']
+    song.name_kana_ruby = row['name_kana_ruby']
+    song.youtube_link = row['youtube']
+  end
 end
