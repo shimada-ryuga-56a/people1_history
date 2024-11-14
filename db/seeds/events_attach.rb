ee43e50aa134ac6e1f8ea9425a81814668f13859
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-14 17:19:13 +0900

    feat: リサイズやめてみた

--- db/seeds/events_attach.rb
+++ db/seeds/events_attach.rb
@@ -5,13 +5,5 @@ CSV.foreach('db/csv/events.csv', headers: true) do |row|
   if event.visual_image.attached?
     return
   end
-  if row['visual_image'].present? && File.exist?(Rails.root.join(row['visual_image']))
-    file = ImageProcessing::MiniMagick.source(File.open(Rails.root.join(row['visual_image']))).resize_to_fit(900, 900).call
-    begin
-    event.visual_image.attach(io: File.open(file.path),filename: File.basename(row['visual_image']))
-    ensure
-    file.close
-    file.unlink
-    end
-  end
+  event.visual_image.attach(io: File.open(Rails.root.join(row['visual_image'])),filename: File.basename(row['visual_image']))
 end
--- db/seeds/songs_attach.rb
+++ db/seeds/songs_attach.rb
@@ -5,13 +5,5 @@ CSV.foreach('db/csv/songs.csv', headers: true) do |row|
   if song.jk.attached?
     return
   end
-  if row['jk'].present? && File.exist?(Rails.root.join(row['jk']))
-    file = ImageProcessing::MiniMagick.source(File.open(Rails.root.join(row['jk']))).resize_to_fit(900, 900).call
-    begin
-    song.jk.attach(io: File.open(file.path),filename: File.basename(row['jk']))
-    ensure
-    file.close
-    file.unlink
-    end
-  end
+  song.jk.attach(io: File.open(Rails.root.join(row['jk'])),filename: File.basename(row['jk']))
 end
