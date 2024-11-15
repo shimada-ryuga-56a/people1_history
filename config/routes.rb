6224121361a4bdc44433906ba2eb62e1e712faef
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-15 10:23:58 +0900

    add: マイページのコントローラ・ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   end
 
   resources :setlistitem_informations, only: [:create]
+  resource :mypage, only: [:show]
 
   # セットリストのオートコンプリートのためのルーティング
   resources :setlists do
