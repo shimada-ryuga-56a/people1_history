7728991bd0081e7d71720edff1dc4648f05a2406
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-16 00:20:57 +0900

    add: フラッシュメッセージ表示

--- app/controllers/event_informations_controller.rb
+++ app/controllers/event_informations_controller.rb
@@ -2,6 +2,7 @@ class EventInformationsController < ApplicationController
   def create
     @info = EventInformation.new(event_information_params)
     if @info.save
+      flash[:success] = "投稿に成功しました"
       redirect_to event_path(id: params[:event_id])
     else
       @event = Event.includes(:setlist).find(params[:id])
@@ -14,6 +15,7 @@ class EventInformationsController < ApplicationController
         @infos << SetlistitemInformation.where(setlistitem_id: item.id)
       end
       @infos.flatten!
+      flash.now[:error] = "投稿に失敗しました"
       render 'events/show', status: :unprocessable_entity
     end
   end
