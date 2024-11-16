66716aa7eb7b707ffc8219ced260c5892e436616
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-16 18:22:22 +0900

    add: ルーティング・コントローラ等へのメソッド記述

--- app/models/event_information.rb
+++ app/models/event_information.rb
@@ -5,4 +5,9 @@ class EventInformation < ApplicationRecord
   has_many :likes_on_event_informations, dependent: :destroy
 
   validates :body, presence: true
+
+
+  def liked_by?(user)
+    likes_on_event_informations.exists?(user_id: user.id)
+  end
 end
