08c62a4e20a183726402934c68ca4ea64f2649c7
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-16 10:24:12 +0900

    fix: バリデーション修正

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ApplicationRecord
   has_one_attached :jk
 
   with_options presence: true do
-    validates :name_hiragana_ruby, format: { with: /^[ぁ-んー－]+$/, message: "全角カタカナで入力して下さい" }
+    validates :name_hiragana_ruby, format: { with: /\A[ぁ-んー－]+\z/, message: "全角カタカナで入力して下さい" }
     validates :name_kana_ruby, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力して下さい" }
   end
 end
