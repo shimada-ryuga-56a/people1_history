5faad62d8782e8c6348ba654ba86bc44f7e8a7eb
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-16 11:06:02 +0900

    add: ふぁぼ関連のリレーション記述

--- app/models/song_information.rb
+++ app/models/song_information.rb
@@ -1,6 +1,7 @@
 class SongInformation < ApplicationRecord
   belongs_to :user
   belongs_to :song
+  has_many :likes_on_song_informations, dependent: :destroy
 
   validates :body, presence: true
 end
