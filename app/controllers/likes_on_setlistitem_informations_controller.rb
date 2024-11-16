5629682892437134fa255235bbb4b032657353f4
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-16 21:01:23 +0900

    add: コントローラ記述

--- app/controllers/likes_on_setlistitem_informations_controller.rb
+++ app/controllers/likes_on_setlistitem_informations_controller.rb
@@ -1,7 +1,15 @@
 class LikesOnSetlistitemInformationsController < ApplicationController
   def create
+    setlistitem_information = SetlistitemInformation.find(params[:setlistitem_information_id])
+    likes_on_setlistitem_information = current_user.likes_on_setlistitem_informations.new(setlistitem_information_id: setlistitem_information.id)
+    likes_on_setlistitem_information.save
+    redirect_to event_path(id: setlistitem_information.setlist.event.id)
   end
 
   def destroy
+    setlistitem_information = SetlistitemInformation.find(params[:setlistitem_information_id])
+    likes_on_setlistitem_information = current_user.likes_on_setlistitem_information.find_by(setlistitem_information_id: setlistitem_information.id)
+    likes_on_setlistitem_information.destroy
+    redirect_to event_path(id: setlistitem_information.setlistitem.event.id)
   end
 end
