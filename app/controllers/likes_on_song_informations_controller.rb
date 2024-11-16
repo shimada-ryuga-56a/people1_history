de1b9450f8fd380ba2a1fca551d43cbe0d41b2ed
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-16 17:16:11 +0900

    feat: ふぁぼ実装

--- app/controllers/likes_on_song_informations_controller.rb
+++ app/controllers/likes_on_song_informations_controller.rb
@@ -3,13 +3,13 @@ class LikesOnSongInformationsController < ApplicationController
     song_information = SongInformation.find(params[:song_information_id])
     likes_on_song_information = current_user.likes_on_song_informations.new(song_information_id: song_information.id)
     likes_on_song_information.save
-    redirect_to request.referer
+    redirect_to song_path(id: song_information.song.id)
   end
 
   def destroy
     song_information = SongInformation.find(params[:song_information_id])
     likes_on_song_information = current_user.likes_on_song_informations.find_by(song_information_id: song_information.id)
     likes_on_song_information.destroy
-    redirect_to request.referer
+    redirect_to song_path(id: song_information.song.id)
   end
 end
