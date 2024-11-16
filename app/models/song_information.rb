e4f528505ab07d63c5c17c9d78bb97cff284021a
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-16 11:47:29 +0900

    feat: ふぁぼ関連のメソッド追記

--- app/models/song_information.rb
+++ app/models/song_information.rb
@@ -4,4 +4,8 @@ class SongInformation < ApplicationRecord
   has_many :likes_on_song_informations, dependent: :destroy
 
   validates :body, presence: true
+
+  def liked_by?(user)
+    likes_on_song_informations.exists?(user_id: user.id)
+  end
 end
