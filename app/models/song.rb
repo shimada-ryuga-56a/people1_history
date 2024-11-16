175f7e049e06db368e993ece0757eee1e48c67eb
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-16 10:18:34 +0900

    feat: 平仮名読カラムのバリデーション記述

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,9 @@
 class Song < ApplicationRecord
   has_many :song_informations, dependent: :destroy
   has_one_attached :jk
+
+  with_options presence: true do
+    validates :name_hiragana_ruby, format: { with: /^[ぁ-んー－]+$/, message: "全角カタカナで入力して下さい" }
+    validates :name_kana_ruby, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力して下さい" }
+  end
 end
