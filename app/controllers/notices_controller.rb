03a25676a2c41dde7d81c553436b695760345f17
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-17 21:54:22 +0900

    add: destroyアクション記述

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -4,5 +4,17 @@ class NoticesController < ApplicationController
   end
 
   def destroy
+    @notices = Notice.where(user_id: current_user.id, unread: true).includes(noticeable: :user).order(created_at: :desc)
+    @notices.update_to_read
+    redirect_to notices_path
+  end
+
+  private
+
+  def update_to_read
+    self.each do |notice|
+      notice.unread = false
+      notice.save
+    end
   end
 end
