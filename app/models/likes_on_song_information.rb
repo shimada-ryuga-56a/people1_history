bfcc038ad2468a83908afd882aad0789e8858e96
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-17 16:53:16 +0900

    add: 楽曲インフォいいね時・解除時にレコード操作

--- app/models/likes_on_song_information.rb
+++ app/models/likes_on_song_information.rb
@@ -3,4 +3,19 @@ class LikesOnSongInformation < ApplicationRecord
   belongs_to :song_information
 
   has_many :notices, as: :noticeable, dependent: :destroy
+
+  after_create :create_notice
+  before_destroy :destroy_notice
+
+  private
+
+  def create_notice
+    @notice = Notice.new(noticeable_type: LikesOnSongInformation, noticeable_id: self.id, user_id: self.user.id, action_type: Notice.action_types[:like])
+    @notice.save
+  end
+
+  def destroy_notice
+    @notice = Notice.find_by(noticeable_type: "LikesOnSongInformation", noticeable_id: self.id, user_id: self.user.id)
+    @notice.destroy if @notice.present?
+  end
 end
