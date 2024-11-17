eeb6ef442c77e80686527a7062a57dfcf8bd5b37
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-17 22:27:15 +0900

    add: 全アクション実行前に通知有無取得

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: { safari: 15.0, chrome: 50, firefox: 121, ie: false }
   before_action :authenticate_user!
+  before_action :any_unread_notice?
+
+  private
+
+  def any_unread_notice?
+    return unless user_signed_in?
+    @notice = Notice.find_by(user_id: current_user, unread: true)
+  end
 end
