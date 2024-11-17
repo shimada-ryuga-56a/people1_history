141c9e11671dbc58982f754c8e160a641c91817f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-17 18:14:09 +0900

    improve: N+1問題改善

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,6 +1,6 @@
 class NoticesController < ApplicationController
   def index
-    @notices = Notice.where(user_id: current_user.id)
+    @notices = Notice.where(user_id: current_user.id).includes(noticeable: :user).order(created_at: :desc)
   end
 
   def destroy
