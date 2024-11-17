7d7de261d5ff7988e8dff3bdb60c04e0cd855b4b
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-17 16:47:11 +0900

    improve: イベントインフォいいね時・解除時にレコード

--- app/controllers/likes_on_event_informations_controller.rb
+++ app/controllers/likes_on_event_informations_controller.rb
@@ -3,14 +3,12 @@ class LikesOnEventInformationsController < ApplicationController
     event_information = EventInformation.find(params[:event_information_id])
     likes_on_event_information = current_user.likes_on_event_informations.new(event_information_id: event_information.id)
     likes_on_event_information.save
-    likes_on_event_information.create_notice(event_information.user)
     redirect_to event_path(id: event_information.event.id)
   end
 
   def destroy
     event_information = EventInformation.find(params[:event_information_id])
     likes_on_event_information = current_user.likes_on_event_informations.find_by(event_information_id: event_information.id)
-    likes_on_event_information.destroy_notice(event_information.user)
     likes_on_event_information.destroy
     redirect_to event_path(id: event_information.event.id)
   end
--- app/models/likes_on_event_information.rb
+++ app/models/likes_on_event_information.rb
@@ -4,13 +4,18 @@ class LikesOnEventInformation < ApplicationRecord
 
   has_many :notices, as: :noticeable, dependent: :destroy
 
-  def create_notice(user)
-    @notice = Notice.new(noticeable_type: LikesOnEventInformation, noticeable_id: self.id, user_id: user.id, action_type: Notice.action_types[:like])
+  after_create :create_notice
+  before_destroy :destroy_notice
+
+  private
+
+  def create_notice
+    @notice = Notice.new(noticeable_type: LikesOnEventInformation, noticeable_id: self.id, user_id: self.user.id, action_type: Notice.action_types[:like])
     @notice.save
   end
 
-  def destroy_notice(user)
-    @notice = Notice.find_by(noticeable_type: "LikesOnEventInformation", noticeable_id: self.id, user_id: user.id)
-    @notice.destroy
+  def destroy_notice
+    @notice = Notice.find_by(noticeable_type: "LikesOnEventInformation", noticeable_id: self.id, user_id: self.user.id)
+    @notice.destroy if @notice.present?
   end
 end
