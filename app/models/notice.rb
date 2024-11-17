42c9a60935c6f360be28d60dbaf1a84f00920a6f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-17 15:03:22 +0900

    add: noticeのaction_type enum記述

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -2,7 +2,5 @@ class Notice < ApplicationRecord
   belongs_to :noticeable, polymorphic: true
   belongs_to :user
 
-  enum action_type: {
-    like:  0
-  }
+  enum :action_type, { liked: 0, commented: 1 }
 end
