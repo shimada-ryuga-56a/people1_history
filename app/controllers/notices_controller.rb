2c45173ec64252343e40c05ebc9bc4d6e191ec48
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-17 21:46:37 +0900

    fix: 未読のみ取得

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,6 +1,6 @@
 class NoticesController < ApplicationController
   def index
-    @notices = Notice.where(user_id: current_user.id).includes(noticeable: :user).order(created_at: :desc)
+    @notices = Notice.where(user_id: current_user.id, unread: true).includes(noticeable: :user).order(created_at: :desc)
   end
 
   def destroy
