107f0ee766c5eb97f02640073926d7fb62e2e830
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-18 16:02:24 +0900

    feat: リハーサル選択できるように修正

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -30,7 +30,7 @@ class SetlistsController < ApplicationController
   def setlist_params
     params.require(:setlist).permit(
       :event_id,
-      setlistitems_attributes: [:song_title, :position, :is_encore, :is_song, :is_arranged, :_destroy]
+      setlistitems_attributes: [:song_title, :position, :is_encore, :is_song, :is_arranged, :is_rehearsal, :_destroy]
     ).merge(user_id: current_user.id, event_id: params[:event_id])
   end
 
