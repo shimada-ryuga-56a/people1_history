ea0345ed1025519563c18aebacf51791c920d6da
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-19 13:37:55 +0900

    feat: インフォの表示整頓

--- app/helpers/setlistitems_helper.rb
+++ app/helpers/setlistitems_helper.rb
@@ -4,6 +4,8 @@ module SetlistitemsHelper
     item.song_title = item.is_arranged? ? "新訳：#{item.song_title}" : item.song_title
     # アンコールの場合
     item.song_title = item.is_encore? ? "En. #{item.song_title}" : item.song_title
+    # リハーサルの場合
+    item.song_title = item.is_rehearsal? ? "（リハ） #{item.song_title}" : item.song_title
     # 楽曲ではない場合
     item.song_title = item.is_song? ? item.song_title : "（#{item.song_title}）"
   end
