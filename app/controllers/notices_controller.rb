31ca9b968e3f82d149d4a76c0a7985ce8fdb8e9e
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-19 10:37:40 +0900

    fix: 通知取得アクションの編集

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,10 +1,10 @@
 class NoticesController < ApplicationController
   def index
-    @notices = Notice.where(from_whom_id: current_user.id, unread: true).includes(noticeable: :user).order(created_at: :desc)
+    @notices = Notice.where(user_id: current_user.id, unread: true).includes(noticeable: :user).order(created_at: :desc)
   end
 
   def destroy
-    @notices = Notice.where(from_whom_id: current_user.id, unread: true).includes(noticeable: :user).order(created_at: :desc)
+    @notices = Notice.where(user_id: current_user.id, unread: true).includes(noticeable: :user).order(created_at: :desc)
     @notices.each do |notice|
       notice.unread = false
       notice.save
