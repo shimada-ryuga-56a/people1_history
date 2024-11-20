012c2322279525bec2ef34cf677122879217561b
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-20 22:55:38 +0900

    feat: 楽曲詳細ページ編集

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,6 +7,6 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
     @song_information = SongInformation.new
     @song_informations = @song.song_informations.includes(:user).order(created_at: :desc)
-    @events = Event.joins(setlist: :setlistitems).where(setlistitems: {song_id: params[:id]}).order(:date)
+    @events = Event.joins(setlist: :setlistitems).where(setlistitems: { song_id: params[:id] }).order(:date)
   end
 end
