e7058e6c30ebd722f5380ed527d8f65362ccb242
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-23 00:41:47 +0900

    feat: フラッシュメッセージ アニメーション 仮設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[top contact]
   def top
+    flash[:success] = I18n.t('flash.success.post')
   end
 
   def contact
