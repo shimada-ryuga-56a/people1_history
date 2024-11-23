884f3efd9a37a510fb3ae416170327555a91af01
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-23 13:27:19 +0900

    fix: ツアーインフォ フォームを空っぽで提出した際のエラー表示

--- app/controllers/tour_informations_controller.rb
+++ app/controllers/tour_informations_controller.rb
@@ -3,10 +3,12 @@ class TourInformationsController < ApplicationController
     @info = TourInformation.new(tour_information_params)
     respond_to do |format|
       if @info.save
+        @new_info = TourInformation.new
         @tour = Tour.find(@info.tour_id)
         flash.now[:success] = I18n.t('flash.success.post')
         format.turbo_stream
       else
+        @new_info = @info
         @tour = Tour.find(params[:tour_id])
         @tours = Event.where(tour_id: params[:tour_id])
         @informations = TourInformation.where(tour_id: params[:id]).order(created_at: 'DESC')
--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -6,6 +6,7 @@ class ToursController < ApplicationController
   def show
     @tour = Tour.find(params[:id])
     @tours = Event.where(tour_id: params[:id])
+    @new_info = TourInformation.new
     @informations = TourInformation.where(tour_id: params[:id]).order(created_at: 'DESC')
   end
 end
