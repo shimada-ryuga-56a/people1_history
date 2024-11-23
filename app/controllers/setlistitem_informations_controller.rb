467acb31c05fd9773eb482456c604dea99b5891f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-23 15:34:34 +0900

    fix: セトリアイテムインフォのエラー表示をTurbo_stream化

--- app/controllers/setlistitem_informations_controller.rb
+++ app/controllers/setlistitem_informations_controller.rb
@@ -1,22 +1,16 @@
 class SetlistitemInformationsController < ApplicationController
   def create
     @info = SetlistitemInformation.new(setlistitem_information_params)
+    @item = Setlistitem.find(@info.setlistitem_id)
     respond_to do |format|
       if @info.save
         @setlistitem_new_info = SetlistitemInformation.new
-        @item = Setlistitem.find(@info.setlistitem_id)
         flash.now[:success] = I18n.t('flash.success.post')
         format.turbo_stream
       else
-        @event = Event.find(@info.setlistitem.setlist.event.id)
-        @event_infomations = EventInformation.where(event_id: @event.id).order(created_at: 'DESC')
-        @setlistitems = Setlistitem.where(setlist_id: @event.setlist.id)
-        @infos = []
-        @setlistitems.each do |item|
-          @infos << SetlistitemInformation.where(setlistitem_id: item.id)
-        end
-        @infos.flatten!
-        format.html { render 'events/show', status: :unprocessable_entity }
+        @setlistitem_new_info = @info
+        format.turbo_stream { render 'create_failure', status: :unprocessable_entity }
+        flash.now[:error] = I18n.t('flash.error.post')
       end
     end
   end
