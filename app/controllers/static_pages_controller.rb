a2378b1291ee56c15728cc1379ac9b4090510c74
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-23 10:10:09 +0900

    fix: 開発用フラッシュメッセージ削除

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[top contact]
   def top
-    flash[:success] = I18n.t('flash.success.post')
   end
 
   def contact
