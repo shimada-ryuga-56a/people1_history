209e38d81b632026e5299aae5c10dda84f1024cd
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-29 15:42:38 +0900

    fix: Rubocop修正

--- app/helpers/setlistitem_informations_helper.rb
+++ app/helpers/setlistitem_informations_helper.rb
@@ -1,2 +1,6 @@
 module SetlistitemInformationsHelper
+  def setlistitem_information_hint
+    "（投稿のヒント）#{['？ボタンで投稿のヒントが見れます！', 'Itoさんが〇〇って言ってた！等', 'トラブルとかもあったら教えて👂', 'メンバーの発言なども教えてください！', '歌詞変えとかあったら教えて！', '楽しかった〜？',
+                        '歌詞間違えてたらこっそり教えて👂'].sample}"
+  end
 end
--- app/helpers/setlistitems_helper.rb
+++ app/helpers/setlistitems_helper.rb
@@ -9,8 +9,4 @@ module SetlistitemsHelper
     # 楽曲ではない場合
     item.song_title = item.is_song? ? item.song_title : "（#{item.song_title}）"
   end
-
-  def setlistitem_information_hint
-    "（投稿のヒント）" + ["？ボタンで投稿のヒントが見れます！", "Itoさんが〇〇って言ってた！等", "トラブルとかもあったら教えて👂", "メンバーの発言なども教えてください！", "歌詞変えとかあったら教えて！", "楽しかった〜？", "歌詞間違えてたらこっそり教えて👂"].sample
-  end
 end
