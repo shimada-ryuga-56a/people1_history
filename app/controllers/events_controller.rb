d3f1c7faf00cadfdb4a6e7189efd43c946b35b28
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-30 09:34:17 +0900

    feat: ログイン前ユーザーが情報の閲覧可能に

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
   def index
     @categories = Event.categories
     @q = Event.ransack(params[:q])
