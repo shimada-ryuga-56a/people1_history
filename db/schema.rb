361d099593ef20d77398f34d382367c146e63627
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-01 00:22:51 +0900

    fix: DiscItemテーブルに新訳カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_30_145557) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_30_152020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_30_145557) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_arranged", default: false, null: false
   end
 
   create_table "disc_versions", force: :cascade do |t|
