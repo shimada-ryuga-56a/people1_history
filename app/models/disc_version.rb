1c26ac59ead7a3ad39938d2284607095c28471b0
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-30 15:10:27 +0900

    add: DiscVersionsテーブル・関連ファイル追加

---
+++ app/models/disc_version.rb
@@ -0,0 +1,2 @@
+class DiscVersion < ApplicationRecord
+end
---
+++ test/models/disc_version_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DiscVersionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
