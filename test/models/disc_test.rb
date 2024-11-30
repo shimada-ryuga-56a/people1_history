92ec4a14684b6696e661e4dab7bf12838b81de35
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-01 00:05:28 +0900

    fix: Rubocop修正

--- test/models/disc_test.rb
+++ test/models/disc_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class DiscTest < ActiveSupport::TestCase
   # test "the truth" do
--- test/models/disc_version_test.rb
+++ test/models/disc_version_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class DiscVersionTest < ActiveSupport::TestCase
   # test "the truth" do
