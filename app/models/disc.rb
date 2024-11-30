c265685416e81693a432dc05557c959cc8e175fd
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-11-30 11:58:38 +0900

    add: モデルファイルにバリデーション追記

--- app/models/disc.rb
+++ app/models/disc.rb
@@ -1,2 +1,6 @@
 class Disc < ApplicationRecord
+  validates :title, presence: true
+  validates :title_ruby, presence: true
+  validates :announcement_date, presence: true
+  validates :type, presence: true
 end
