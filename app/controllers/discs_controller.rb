56df182d81a205b5515ee0d49e7625b3e6d00eb9
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-03 00:04:52 +0900

    fix: インスタンス変数読み込みエラー解消

--- app/controllers/discs_controller.rb
+++ app/controllers/discs_controller.rb
@@ -2,7 +2,7 @@ class DiscsController < ApplicationController
   def show
     @disc = Disc.find(params[:id])
     @disc_versions = DiscVersion.includes(:disc).where(disc_id: params[:id])
-    @disc_contents = DiscContent.includes(disc_version: :disc).where(disc: {disc_id: params[:id]})
+    @disc_contents = DiscContent.includes(disc_version: :disc).where(disc: {id: params[:id]})
     @disc_items = DiscItem.includes(disc_content: [:event, { disc_version: :disc }]).where(disc: {id: params[:id]})
   end
 end
