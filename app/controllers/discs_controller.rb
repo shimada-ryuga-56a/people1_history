a146697f516e0af0fdf3c697ece89ac1ee1cd88d
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-02 20:44:06 +0900

    add: コントローラでdisc_idをもとにしたアイテム取得

--- app/controllers/discs_controller.rb
+++ app/controllers/discs_controller.rb
@@ -1,4 +1,6 @@
 class DiscsController < ApplicationController
   def show
+    @disc_items = DiscItem.includes(disc_content: { disc_version: :disc }).where(disc: {id: params[:id]})
+    p @disc_items
   end
 end
