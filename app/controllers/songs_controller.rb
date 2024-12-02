51408fa20f02dabf7c8fedc3305580d5622a3f4f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-02 18:59:00 +0900

    improve: 1+N問題解決

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,8 +9,8 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
     @new_song_information = SongInformation.new
     @song_informations = @song.song_informations.includes(:user).order(created_at: :desc)
-    @events = Event.joins(setlist: :setlistitems).where(setlistitems: { song_id: params[:id] }).order(:date)
+    @events = Event.includes(setlist: :setlistitems).where(setlistitems: { song_id: params[:id] }).order(:date)
     @song_disc_items = DiscItem.includes(disc_content: {disc_version: :disc}).where(disc_contents: {content_type: 0}, disc_items: {song_id: params[:id]}).decorate
-    @movie_disc_items = DiscItem.includes(disc_content: {disc_version: :disc}).where(disc_contents: {content_type: [1, 2]}, disc_items: {song_id: params[:id]}).decorate
+    @movie_disc_items = DiscItem.includes(disc_content: [:event, {disc_version: :disc}]).where(disc_contents: {content_type: [1, 2]}, disc_items: {song_id: params[:id]}).decorate
   end
 end
