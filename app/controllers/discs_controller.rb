82e295ea91920d6c2dec914087169861a20e23ee
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-02 20:51:19 +0900

    add: @discカラムにdisc情報格納

--- app/controllers/discs_controller.rb
+++ app/controllers/discs_controller.rb
@@ -1,5 +1,6 @@
 class DiscsController < ApplicationController
   def show
+    @disc = Disc.find(params[:id])
     @disc_items = DiscItem.includes(disc_content: [:event, { disc_version: :disc }]).where(disc: {id: params[:id]})
   end
 end
