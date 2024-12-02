0ee4752f1fdbddcfa3c4fc2391da94f14211c4a5
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-02 20:45:36 +0900

    improve: showアクションでイベント情報も取得するよう変更

--- app/controllers/discs_controller.rb
+++ app/controllers/discs_controller.rb
@@ -1,6 +1,5 @@
 class DiscsController < ApplicationController
   def show
-    @disc_items = DiscItem.includes(disc_content: { disc_version: :disc }).where(disc: {id: params[:id]})
-    p @disc_items
+    @disc_items = DiscItem.includes(disc_content: [:event, { disc_version: :disc }]).where(disc: {id: params[:id]})
   end
 end
