11bb4293583a4e544161f826556b517f9044a67f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-08 15:57:29 +0900

    feat: 楽曲名での検索を実現

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,4 +7,12 @@ class Song < ApplicationRecord
     validates :name_hiragana_ruby, format: { with: /\A[ぁ-んー－]+\z/, message: I18n.t('model.song.validates.hiragana_ruby') }
     validates :name_kana_ruby, format: { with: /\A[ァ-ヶー－]+\z/, message: I18n.t('model.song.validates.kana_ruby') }
   end
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[name name_kana_ruby name_hiragana_ruby]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[name name_kana_ruby name_hiragana_ruby]
+  end
 end
