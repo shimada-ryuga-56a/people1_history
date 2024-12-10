52f0e74fa26cb1a21546a2a4b86a384af2786660
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-10 17:23:12 +0900

    add: ブックマークのcreate, destroyアクション設定

--- app/controllers/event_bookmarks_controller.rb
+++ app/controllers/event_bookmarks_controller.rb
@@ -1,7 +1,15 @@
 class EventBookmarksController < ApplicationController
   def create
+    event = Event.find(params[:event_id])
+    event_bookmark = current_user.event_bookmarks.new(event_id: event.id)
+    event_bookmarks.save
+    redirect_to event_path(id: event.id)
   end
 
   def destroy
+    event = Event.find(params[:event_id])
+    event_bookmark = current_user.event_bookmarks.find_by(event_id: event.id)
+    event_bookmark.destroy
+    redirect_to event_path(id: event.id)
   end
 end
