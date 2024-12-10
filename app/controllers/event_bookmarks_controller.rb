b4ee2dfb8fc9e6c942cc4ca646a704b87732de1c
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-10 22:03:38 +0900

    feat: 非同期処理のためにコントローラ修正

--- app/controllers/event_bookmarks_controller.rb
+++ app/controllers/event_bookmarks_controller.rb
@@ -1,14 +1,18 @@
 class EventBookmarksController < ApplicationController
   def create
-    event = Event.find(params[:event_id])
-    event_bookmark = current_user.event_bookmarks.new(event_id: event.id)
+    @event = Event.find(params[:event_id])
+    event_bookmark = current_user.event_bookmarks.new(event_id: @event.id)
     event_bookmark.save
-    redirect_to request.referer
+    respond_to do |format|
+      format.turbo_stream { render 'events/bookmark', locals: {event: @event} }
+    end
   end
 
   def destroy
     event_bookmark = current_user.event_bookmarks.find(params[:id])
     event_bookmark.destroy
-    redirect_to request.referer
+    respond_to do |format|
+      format.turbo_stream { render 'events/bookmark', locals: {event: Event.find(event_bookmark.event.id)} }
+    end
   end
 end
