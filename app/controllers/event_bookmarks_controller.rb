abf0f6a1f5c6558cf84948864f2133e01567b212
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-10 20:34:44 +0900

    fix: コントローラ修正

--- app/controllers/event_bookmarks_controller.rb
+++ app/controllers/event_bookmarks_controller.rb
@@ -2,14 +2,13 @@ class EventBookmarksController < ApplicationController
   def create
     event = Event.find(params[:event_id])
     event_bookmark = current_user.event_bookmarks.new(event_id: event.id)
-    event_bookmarks.save
-    redirect_to event_path(id: event.id)
+    event_bookmark.save
+    redirect_to request.referer
   end
 
   def destroy
-    event = Event.find(params[:event_id])
-    event_bookmark = current_user.event_bookmarks.find_by(event_id: event.id)
+    event_bookmark = current_user.event_bookmarks.find(params[:id])
     event_bookmark.destroy
-    redirect_to event_path(id: event.id)
+    redirect_to request.referer
   end
 end
