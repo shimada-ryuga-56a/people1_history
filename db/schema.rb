3d44f3e3c4fdf9b97c58ac837b640174bf564377
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-10 15:24:10 +0900

    feat: マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_30_152020) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_10_062209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -74,6 +74,13 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_30_152020) do
     t.date "announcement_date", null: false
     t.date "release_date"
     t.integer "production_type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "event_bookmarks", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
