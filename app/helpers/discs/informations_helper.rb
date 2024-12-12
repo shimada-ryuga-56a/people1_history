0019072ad4c9e6c808ae8d1575c2831ce190ae95
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-12 22:04:31 +0900

    fix: placeholderに表示する文言を変更

--- app/helpers/discs/informations_helper.rb
+++ app/helpers/discs/informations_helper.rb
@@ -1,2 +1,6 @@
 module Discs::InformationsHelper
+  def disc_information_hint
+    "（投稿のヒント）#{['メンバーが話してたエピソードとかを教えてください！', 'ライブのMCとかで話してたエピソードあれば教えてください！', 'メンバーがSNSでこの作品について触れてたら教えてください！',
+                        'インタビューで話されてたエピソードなども教えてください！'].sample}"
+  end
 end
