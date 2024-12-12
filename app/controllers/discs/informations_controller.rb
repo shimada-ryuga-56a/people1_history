4df00e9fa6c4293e773ea5f992e565465fac54e6
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-12 21:52:59 +0900

    fix: インフォ投稿非同期化のための記述に変更

--- app/controllers/discs/informations_controller.rb
+++ app/controllers/discs/informations_controller.rb
@@ -1,13 +1,16 @@
 class Discs::InformationsController < ApplicationController
   def create
-    disc = Disc.find(params[:disc_id])
-    @new_info = disc.informations.build(information_params)
-    if @new_info.save
-      p "保存できた"
-      p @new_info
-    else
-      p "保存できてない"
-      p @new_info
+    @disc = Disc.find(params[:disc_id])
+    @info = @disc.informations.build(information_params)
+    respond_to do |format|
+      if @info.save
+        @new_info = @disc.informations.build
+        @form_url = disc_informations_path(disc_id: @disc.id)
+        flash.now[:success] = I18n.t('flash.success.post')
+        format.turbo_stream { render 'informations/create' }
+      else
+        format.turbo_stream { render 'informations/create_failure', status: :unprocessable_entity }
+      end
     end
   end
 
