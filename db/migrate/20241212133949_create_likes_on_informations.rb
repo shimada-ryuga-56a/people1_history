acaf7d7c51fc0974fd28c068a51ae025749a9a71
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-12 22:43:08 +0900

    fix: マイグレーションファイルにnull制約記述

--- db/migrate/20241212133949_create_likes_on_informations.rb
+++ db/migrate/20241212133949_create_likes_on_informations.rb
@@ -1,8 +1,8 @@
 class CreateLikesOnInformations < ActiveRecord::Migration[8.0]
   def change
     create_table :likes_on_informations do |t|
-      t.integer :information_id
-      t.integer :user_id
+      t.integer :information_id, null: false
+      t.integer :user_id, null: false
 
       t.timestamps
     end
