1308dda7c7936900c816412b27c5fe1a78d884ab
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-12 23:13:15 +0900

    add: (仮)いいね追加・削除アクション記述

--- app/controllers/likes_on_informations_controller.rb
+++ app/controllers/likes_on_informations_controller.rb
@@ -1,7 +1,13 @@
 class LikesOnInformationsController < ApplicationController
   def create
+    @info = Information.find(params[:information_id])
+    likes_on_information = current_user.likes_on_informations.new(information_id: @info.id)
+    likes_on_information.save
   end
 
   def destroy
+    @info = Information.find(params[:information_id])
+    likes_on_information = current_user.likes_on_informations.find_by(information_id: @info.id)
+    likes_on_information.destroy
   end
 end
