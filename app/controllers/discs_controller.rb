1e37eff35f224ca7ee88dc9b05ede1a5f33838b2
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-12 17:34:13 +0900

    add: フォーム用インスタンス生成

--- app/controllers/discs_controller.rb
+++ app/controllers/discs_controller.rb
@@ -4,6 +4,7 @@ class DiscsController < ApplicationController
     @disc_versions = DiscVersion.includes(:disc).where(disc_id: params[:id])
     @disc_contents = DiscContent.includes(:event, disc_version: :disc).where(disc: { id: params[:id] }).order(:id)
     @disc_items = DiscItem.includes(:song, disc_content: [:event, { disc_version: :disc }]).where(disc: { id: params[:id] })
+    @new_info = @disc.informations.build
   end
 
   def jacket
