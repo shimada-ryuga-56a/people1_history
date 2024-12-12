63b3a8020f9c299176e70259a9b4100969f07777
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-12 17:38:14 +0900

    add: (仮)コントローラ記述 デバッグ用コード記述

--- app/controllers/discs/informations_controller.rb
+++ app/controllers/discs/informations_controller.rb
@@ -1,4 +1,19 @@
 class Discs::InformationsController < ApplicationController
   def create
+    disc = Disc.find(params[:disc_id])
+    @new_info = disc.informations.build(information_params)
+    if @new_info.save
+      p "保存できた"
+      p @new_info
+    else
+      p "保存できてない"
+      p @new_info
+    end
+  end
+
+  private
+
+  def information_params
+    params.require(:information).permit(:body).merge(user_id: current_user.id)
   end
 end
