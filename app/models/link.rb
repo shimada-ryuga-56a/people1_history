2a0df1db046ddaa639911209f28864ba957f813e
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-13 15:26:20 +0900

    fix: platformの種類変更 （一旦重要なものだけを登録）

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,5 +3,5 @@ class Link < ApplicationRecord
   validates :url_link, presence: true
   validates :remark, presence: true
 
-  enum :platform, { YouTube: 0, YouTubeMusic: 1, AppleMusic: 2, Spotify: 3, TikTok: 4, X: 5 }
+  enum :platform, { YouTube: 0, YouTubeMusic: 1, TikTok: 2, X: 3 }
 end
