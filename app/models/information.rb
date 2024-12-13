5716093a5429455ffc7455d209bf0e99e3afa5fc
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-13 09:28:10 +0900

    add: InformationとNoticeの関連付け・いいねと同時にNoticeレコード登録

--- app/models/information.rb
+++ app/models/information.rb
@@ -2,6 +2,7 @@ class Information < ApplicationRecord
   belongs_to :reportable, polymorphic: true
   belongs_to :user
   has_many :likes_on_informations, dependent: :destroy
+  has_many :notices, as: :noticeable, dependent: :destroy
 
   validates :body, presence: true
 
