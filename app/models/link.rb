08993672d3d7c94ac2e07aaf54893bb6590a97ba
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-13 13:16:56 +0900

    add: バリデーション記述・enum値設定

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,7 @@
 class Link < ApplicationRecord
+  validates :platform, presence: true
+  validates :url_link, presence: true
+  validates :remark, presence: true
+
+  enum :platform, { YouTube: 0, YouTubeMusic: 1, AppleMusic: 2, Spotify: 3, TikTok: 4, X: 5 }
 end
