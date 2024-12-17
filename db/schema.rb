910a661867e3e89c3769f55136007ef9fc6452af
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-17 12:00:39 +0900

    add: discのrelease_dateにnull制約追加 （先ほどと同様の理由）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_17_025235) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_17_025816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -72,7 +72,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_17_025235) do
     t.string "title", null: false
     t.string "title_ruby", null: false
     t.date "announcement_date", null: false
-    t.date "release_date"
+    t.date "release_date", null: false
     t.integer "production_type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
