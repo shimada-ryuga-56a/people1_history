9f2f8a3dc6e54e267e8fd377545f877f1c2f4d81
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-17 10:47:20 +0900

    add: indexアクションでのデータ所得処理を記述

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,4 +1,15 @@
 class HistoriesController < ApplicationController
   def index
+    @histories = Array.new
+    @histories << Event.all
+    @histories << Disc.all
+    @histories.flatten!.sort_by! { |history|
+      if history.respond_to?("date")
+        history.date
+      else
+        history.release_date
+      end
+    }
+    @histories = Kaminari.paginate_array(@histories).page(params[:page]).per(20)
   end
 end
