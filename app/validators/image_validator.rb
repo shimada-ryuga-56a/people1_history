42b004b0dcece2c4aa12c7b005d225afa9dbf900
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-23 17:55:33 +0900

    fix: エラーメッセージをlocalesファイルから参照

--- app/validators/image_validator.rb
+++ app/validators/image_validator.rb
@@ -1,14 +1,13 @@
 class ImageValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    p "デバッグ用、attribute：#{value.content_type}"
-    return unless record.image.attached?
+    return if value.blank?
 
-    unless ["image/jpeg", "image/jpg", "image/png", "image/heic"].include?(value.content_type)
-      record.errors.add(:image, "が不正なサイズ形式です。")
+    unless ["image/jpeg", "image/jpg", "image/png"].include?(value.content_type)
+      record.errors.add(:image, :invalid_format)
     end
 
     if value.byte_size > 5_1024_1024
-      record.errors.add(:image, "は5MB以内にしてください。")
+      record.errors.add(:image, :too_big_size)
     end
   end
 end
