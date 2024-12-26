8f732479554cda796a3d62e513c7dde75826dd51
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-26 21:20:25 +0900

    add: edit, updateのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
   delete "notices" => "notices#destroy"
 
   resources :members, only: [:index]
-  resources :histories, only: [:index, :new, :create, :show, :destroy]
+  resources :histories, only: [:index, :new, :create, :show, :destroy, :edit, :update]
 
   resources :discs, only: [:show] do
     resources :informations, only: [:create], module: :discs
