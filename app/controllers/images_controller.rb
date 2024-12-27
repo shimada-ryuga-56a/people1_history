809626f441c53cbf392a1a2bb51e3546a9b772b0
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-27 11:10:55 +0900

    add: 画像削除アクションを作成

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,8 @@
 class ImagesController < ApplicationController
-  def destroy
+  def history_image_destroy
+    @history = current_user.histories.find(params[:history_id])
+    @image = @history.image
+    @image.purge
+    redirect_to edit_history_path(@history)
   end
 end
