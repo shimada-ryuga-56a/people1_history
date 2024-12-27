256c6281872ee2b2b829b0998e00f2c217329af9
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-27 23:11:09 +0900

    add: Likeモデルとの関連付け定義

--- app/models/history.rb
+++ app/models/history.rb
@@ -2,6 +2,7 @@ class History < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :informations, as: :reportable, dependent: :destroy
+  has_many :likes, as: :likable, dependent: :destroy
 
   validates :title, presence: true
   validates :date, presence: true
