134ca9ef5571ecef593b9c1fd0a8f8a91f2ac4bb
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-28 13:27:12 +0900

    fix: turbo_frame用の値受け渡し設定・変数名変更

--- app/controllers/histories/likes_controller.rb
+++ app/controllers/histories/likes_controller.rb
@@ -1,10 +1,11 @@
 class Histories::LikesController < ApplicationController
   def create
     @history = History.preload(:likes).find(params[:history_id])
-    likes = @history.likes.new(user_id: current_user.id)
-    likes.save
+    like = @history.likes.new(user_id: current_user.id)
+    like.save
+    obj = @history
     respond_to do |format|
-      format.turbo_stream { render 'likes/create' }
+      format.turbo_stream { render 'likes/create', locals: { obj: @history } }
     end
   end
 
