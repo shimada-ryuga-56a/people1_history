93cb6df34f1c2e161196a7579f4333b4997fbd50
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-28 14:05:38 +0900

    add: いいね解除アクション追加

--- app/controllers/histories/likes_controller.rb
+++ app/controllers/histories/likes_controller.rb
@@ -1,14 +1,19 @@
 class Histories::LikesController < ApplicationController
   def create
-    @history = History.preload(:likes).find(params[:history_id])
+    @history = History.find(params[:history_id])
     like = @history.likes.new(user_id: current_user.id)
     like.save
-    obj = @history
     respond_to do |format|
-      format.turbo_stream { render 'likes/create', locals: { obj: @history } }
+      format.turbo_stream { render 'likes/create', locals: { obj: @history, history: @history } }
     end
   end
 
   def destroy
+    @history = History.find(params[:history_id])
+    like = @history.likes.find_by(user_id: current_user.id, likable_id: @history.id)
+    like.destroy
+    respond_to do |format|
+      format.turbo_stream { render 'likes/destroy', locals: { obj: @history, history: @history } }
+    end
   end
 end
