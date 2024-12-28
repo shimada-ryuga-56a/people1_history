6d6882db5bf458a39408c2ce80414165a7f8a366
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-28 23:48:51 +0900

    fix: userのログインが行えるかを確認

--- test/controllers/mypages_controller_test.rb
+++ test/controllers/mypages_controller_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class MypagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get show' do
+    setup do
+      @user = users(:one)
+      sign_in @user
+    end
+
     get mypage_url
     assert_response :success
   end
