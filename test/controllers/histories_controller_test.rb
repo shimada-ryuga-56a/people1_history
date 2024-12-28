3932c1b4e0b9a25197e985436625a1e206ce7e80
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-28 23:25:21 +0900

    fix: url修正

--- test/controllers/histories_controller_test.rb
+++ test/controllers/histories_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HistoriesControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get histories_index_url
+    get histories_url
     assert_response :success
   end
 end
