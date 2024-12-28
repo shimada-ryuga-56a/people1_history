04c6fe96d8d97d3172bd57927fb003b5dbe50dff
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-28 16:12:53 +0900

    fix: url名変更

--- test/controllers/likes_on_setlistitem_informations_controller_test.rb
+++ test/controllers/likes_on_setlistitem_informations_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class LikesOnSetlistitemInformationsControllerTest < ActionDispatch::IntegrationTest
   test 'should get create' do
-    get likes_on_setlistitem_informations_create_url
+    post setlistitem_information_likes_on_setlistitem_informations_url(setlistitem_information_id: 1)
     assert_response :success
   end
 
   test 'should get destroy' do
-    get likes_on_setlistitem_informations_destroy_url
+    delete setlistitem_information_likes_on_setlistitem_informations_url(setlistitem_information_id: 1)
     assert_response :success
   end
 end
