97b036106eb9978f668b87befb588329f197a80a
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-29 15:48:17 +0900

    fix: セトリインフォ投稿を記述

--- test/controllers/setlistitem_informations_controller_test.rb
+++ test/controllers/setlistitem_informations_controller_test.rb
@@ -4,10 +4,15 @@ class SetlistitemInformationsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
     sign_in @user
+    @setlistitem = create(:setlistitem)
   end
 
   test 'should get create' do
-    post setlistitem_informations_url
+    post "#{setlistitem_informations_url(setlistitem: @setlistitem.id)}.turbo_stream", params: {
+      song_information: {
+        body: 'Sample body text'
+      }
+    }
     assert_response :success
   end
 end
