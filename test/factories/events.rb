43b1ac5d7ec26bf20794b7b06af849451021d978
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-29 14:30:08 +0900

    fix: バリデーションにそう形に変更

--- test/factories/events.rb
+++ test/factories/events.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :event do
     category { 1 }
     name { 'MyText' }
-    name_kana_ruby { 'MyText' }
+    name_kana_ruby { 'テスト' }
     date { '2024-01-01' }
   end
 end
