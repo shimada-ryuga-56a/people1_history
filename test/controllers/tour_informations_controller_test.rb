3e8e7a91f28efab2811c6c75f45cdb9082e9ba5b
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-29 15:45:26 +0900

    add: ツアーインフォ テスト記述

--- test/controllers/tour_informations_controller_test.rb
+++ test/controllers/tour_informations_controller_test.rb
@@ -4,10 +4,15 @@ class TourInformationsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
     sign_in @user
+    @tour = create(:tour)
   end
 
   test 'should get create' do
-    post tour_tour_informations_url(tour_id: 1)
+    post "#{tour_tour_informations_url(tour_id: @tour.id)}.turbo_stream", params: {
+      tour_information: {
+        body: 'Sample body text'
+      }
+    }
     assert_response :success
   end
 end
