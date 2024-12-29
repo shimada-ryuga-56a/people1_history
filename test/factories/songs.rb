7ace6082edd6df82f482884f8a36cb9417d6e0eb
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-29 14:51:49 +0900

    add: バリデーションエラー回避

--- test/factories/songs.rb
+++ test/factories/songs.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :song do
     id { 1 }
     name { 'MyText' }
-    name_kana_ruby { 'MyText' }
+    name_kana_ruby { 'テスト' }
+    name_hiragana_ruby { 'てすと' }
   end
 end
