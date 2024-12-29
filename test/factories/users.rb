d7f74a63c2306827bf82d2a8d72bfaacaef385f6
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-29 22:06:05 +0900

    fix: userの作成も元に戻した

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,14 +1,7 @@
 FactoryBot.define do
-  password = Faker::Internet.password(min_length: 6, max_length: 8)
-
   factory :user do
-    email { Faker::Internet.email } #=> "eliza@mann.net"
-    password { password }
-    password_confirmation { password }
-    last_name { Faker::Lorem.characters(number: 5) } #=> 5文字生成
-    first_name { Faker::Lorem.characters(number: 5) } #=> 5文字生成
-    nickname { Faker::Lorem.characters(number: 5) } #=> 5文字生成
-    introduction { Faker::Lorem.characters(number: 30) } #=> 30文字生成
-    is_deleted { false } #=> boolen型
+    id { 1 }
+    email { 'example@email.com' }
+    password { 'password' }
   end
 end
