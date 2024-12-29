78d54da78345c152e7d53ab8ecbcbd0b89deafcc
shimada-ryuga-56a <shimadaryuga@gmail.com>
2024-12-29 17:12:47 +0900

    fix: ユーザー名の定義

--- test/controllers/likes_on_tour_informations_controller_test.rb
+++ test/controllers/likes_on_tour_informations_controller_test.rb
@@ -5,16 +5,16 @@ class LikesOnTourInformationsControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
     sign_in @user
     @tour = create(:tour)
-    @tour_information = create(:tour_information)
+    @tour_information = create(:tour_information, tour: @tour)
   end
 
   test 'should get create' do
-    post tour_information_likes_on_tour_informations_url(tour_information_id: 1).turbo_stream.to_s
+    post tour_information_likes_on_tour_informations_url(tour_information_id: @tour_information.id).turbo_stream.to_s
     assert_response :success
   end
 
   test 'should get destroy' do
-    post tour_information_likes_on_tour_informations_url(tour_information_id: 1).turbo_stream.to_s
+    post tour_information_likes_on_tour_informations_url(tour_information_id: @tour_information.id).turbo_stream.to_s
     delete tour_information_likes_on_tour_informations_url(tour_information_id: 1).turbo_stream.to_s
     assert_response :success
   end
