7e6200a5ca7467ac7fce4de093498f48e01113e0
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-01 17:54:01 +0900

    fix: Rubocop修正

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,11 +7,11 @@ class Link < ApplicationRecord
 
   enum :platform, { YouTube: 0, YouTubeMusic: 1, TikTok: 2, X: 3 }
 
-  def self.ransackable_associations(auth_object = nil)
-    ["link_contents"]
+  def self.ransackable_associations(_auth_object = nil)
+    ['link_contents']
   end
 
-  def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "id", "id_value", "platform", "remark", "updated_at", "url_link"]
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at id id_value platform remark updated_at url_link]
   end
 end
--- app/models/link_content.rb
+++ app/models/link_content.rb
@@ -2,7 +2,7 @@ class LinkContent < ApplicationRecord
   belongs_to :linkable, polymorphic: true
   belongs_to :link
 
-  def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "id", "id_value", "platform", "remark", "updated_at", "url_link"]
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at id id_value platform remark updated_at url_link]
   end
 end
