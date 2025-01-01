97a7d7aff7d989222f804777e8c141c225bc7030
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-01 17:52:21 +0900

    add: Linkを管理画面で管理できるように追記

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,4 +6,12 @@ class Link < ApplicationRecord
   validates :remark, presence: true
 
   enum :platform, { YouTube: 0, YouTubeMusic: 1, TikTok: 2, X: 3 }
+
+  def self.ransackable_associations(auth_object = nil)
+    ["link_contents"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "platform", "remark", "updated_at", "url_link"]
+  end
 end
--- app/models/link_content.rb
+++ app/models/link_content.rb
@@ -1,4 +1,8 @@
 class LinkContent < ApplicationRecord
   belongs_to :linkable, polymorphic: true
   belongs_to :link
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "platform", "remark", "updated_at", "url_link"]
+  end
 end
