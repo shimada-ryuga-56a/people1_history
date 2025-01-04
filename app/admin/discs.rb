abb09506ad5b4cfb02d8837b23c3da277d8e0cee
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-04 11:44:01 +0900

    add: discのattributesの編集を許可

--- app/admin/discs.rb
+++ app/admin/discs.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register Disc do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :title_ruby, :announcement_date, :release_date, :production_type
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :title_ruby, :announcement_date, :release_date, :production_type]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :title, :title_ruby, :announcement_date, :release_date, :production_type
 end
