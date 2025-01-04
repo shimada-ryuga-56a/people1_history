ccd3c8d94dd352963f82340bc68f65ef878a650d
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-04 15:42:01 +0900

    add: disc_content一覧ページカスタマイズ

--- app/admin/disc_contents.rb
+++ app/admin/disc_contents.rb
@@ -1,6 +1,16 @@
 ActiveAdmin.register DiscContent do
   permit_params :disc_version_id, :event_id, :content_type
 
+  index do
+    id_column
+    column :disc_version
+    column :content_type
+    column :event
+    column :created_at
+    column :updated_at
+    actions
+  end
+
   form do |f|
     f.inputs do
       f.input :disc_version_id
