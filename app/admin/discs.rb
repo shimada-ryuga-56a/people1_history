aa09d67319d06b7b6ece629a10cdabce3471cca4
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-04 15:23:01 +0900

    fix: Disc追加時にDiscVersionsも追加できる形に変更

--- app/admin/discs.rb
+++ app/admin/discs.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register Disc do
-  permit_params :title, :title_ruby, :announcement_date, :release_date, :production_type
+  permit_params :title, :title_ruby, :announcement_date, :release_date, :production_type, disc_versions_attributes: [:id, :version, :price, :_destroy]
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :title_ruby
+      f.input :production_type
+      f.input :announcement_date
+      f.input :release_date
+    end
+
+    f.inputs do
+      f.has_many :disc_versions, allow_destroy: true do|t|
+        t.input :version
+        t.input :price
+      end
+    end
+
+    f.actions
+  end
+
 end
