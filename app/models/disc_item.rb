71df60b7241b9f56c16ec88107ce0c8b6b90ec6f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-04 14:40:24 +0900

    fix: Rubocop修正

--- app/models/disc_item.rb
+++ app/models/disc_item.rb
@@ -6,17 +6,17 @@ class DiscItem < ApplicationRecord
   validates :is_arranged, inclusion: { in: [true, false] }
   validate :required_either_song_or_title
 
-  private
-
-  def required_either_song_or_title
-    nil if song_id.present? ^ title.present?
-  end
-
   def self.ransackable_attributes(_auth_object = nil)
     %w[created_at disc_content_id id id_value is_arranged position song_id title updated_at]
   end
 
   def self.ransackable_associations(_auth_object = nil)
     %w[disc_content song]
+  end
+
+  private
+
+  def required_either_song_or_title
+    nil if song_id.present? ^ title.present?
   end
 end
