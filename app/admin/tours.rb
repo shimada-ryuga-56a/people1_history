406276b95f99294b7a338d6ec68d3f2f25d373e0
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-05 15:47:36 +0900

    add: ツアー登録時にイベントも登録するフォームを追加

--- app/admin/tours.rb
+++ app/admin/tours.rb
@@ -2,4 +2,26 @@ ActiveAdmin.register Tour do
   remove_filter :events
 
   permit_params :name, :name_kana_ruby, events_attributes: [:category, :name, :name_kana_ruby, :date, :place, :place_prefecture, :remark, :is_canceled, :_destroy]
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :name_kana_ruby
+    end
+
+    f.inputs do
+      f.has_many :events, allow_destroy: true do |t|
+        t.input :name
+        t.input :name_kana_ruby
+        t.input :date
+        t.input :category
+        t.input :place_prefecture
+        t.input :place
+        t.input :is_canceled
+        t.input :remark
+      end
+    end
+
+    f.actions
+  end
 end
