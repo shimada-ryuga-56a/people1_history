41235c1405d8e1321d03ebf1e90c428472cd3473
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-06 17:56:11 +0900

    add: イベントの管理画面にリンクの登録フォームを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,11 @@ class Event < ApplicationRecord
   has_many :event_bookmarks, dependent: :destroy
   has_many :disc_contents, dependent: :destroy
   has_one :setlist, dependent: :destroy
+
   has_many :link_contents, as: :linkable, dependent: :destroy
+  has_many :links, through: :link_contents
+  accepts_nested_attributes_for :link_contents, allow_destroy: true
+
   has_one_attached :visual_image
 
   validates :category, presence: true
