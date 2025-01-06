d1c0033343a85236acf89d141c97e72ed7ea9fd7
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-06 15:40:29 +0900

    add: ツアー管理画面にリンクのフォームを追加

--- app/admin/tours.rb
+++ app/admin/tours.rb
@@ -1,15 +1,22 @@
 ActiveAdmin.register Tour do
-  remove_filter :events
+  remove_filter :events, :links
 
   permit_params :name, :name_kana_ruby,
                 events_attributes: [:category, :name, :name_kana_ruby, :date, :place, :place_prefecture,
-                                    :remark, :is_canceled, :_destroy]
+                                    :remark, :is_canceled, :_destroy],
+                link_contents_attributes: [:id, :link_id, :_destroy]
   menu parent: 'Event'
 
   form do |f|
     f.inputs do
       f.input :name
       f.input :name_kana_ruby
+    end
+
+    f.inputs do
+      f.has_many :link_contents, allow_destroy: true do |t|
+        t.input :link_id, as: :select, collection: Link.all.order(remark: :asc).map { |x| [x.remark, x.id] }
+      end
     end
 
     f.inputs do
