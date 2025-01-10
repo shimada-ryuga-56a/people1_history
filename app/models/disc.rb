94b9148ae62b3e12c14d5cdf95c9ee960f7c2c8c
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-10 17:22:53 +0900

    fix: modelのバリデーション削除

--- app/models/disc.rb
+++ app/models/disc.rb
@@ -12,7 +12,6 @@ class Disc < ApplicationRecord
 
   validates :title, presence: true
   validates :title_ruby, presence: true
-  validates :release_date, presence: true
   validates :production_type, presence: true
 
   enum :production_type, { single: 0, ep: 1, album: 2, movie: 3 }
