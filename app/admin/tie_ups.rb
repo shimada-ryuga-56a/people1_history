3c0322bd188a072da1bbca33f46d1e3138a669ce
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-10 12:00:49 +0900

    add: 管理画面でのis_new_songの扱い記述

--- app/admin/tie_ups.rb
+++ app/admin/tie_ups.rb
@@ -1,4 +1,4 @@
 ActiveAdmin.register TieUp do
   remove_filter :image_attachment, :image_blob
-  permit_params :collaboration_title, :song_id, :announce_date, :remark, :jacket, :remove_jacket
+  permit_params :collaboration_title, :song_id, :announce_date, :remark, :is_new_song, :jacket, :remove_jacket
 end
