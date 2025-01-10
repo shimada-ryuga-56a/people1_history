36f7d8afaf662d19fc1df363a3b90e7868ecafbe
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-10 17:13:08 +0900

    fix: Rubocop修正

--- app/models/disc_date.rb
+++ app/models/disc_date.rb
@@ -1,17 +1,16 @@
 class DiscDate < ApplicationRecord
   validates :date, presence: true
-  validates :disc_id, presence: true
   validates :date_type, presence: true
 
   belongs_to :disc
 
   enum :date_type, { announcement: 0, release: 1, postponement: 2 }
 
-  def self.ransackable_associations(auth_object = nil)
-    ["disc"]
+  def self.ransackable_associations(_auth_object = nil)
+    ['disc']
   end
 
-  def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "date", "date_type", "disc_id", "id", "remark", "updated_at"]
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at date date_type disc_id id remark updated_at]
   end
 end
