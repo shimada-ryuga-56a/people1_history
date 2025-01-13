cccacf7746d67f4a1e1174368fad53b3d84b548f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-13 13:23:04 +0900

    fix: リンク日付関連 管理画面が機能するよう修正

--- app/models/link_date.rb
+++ app/models/link_date.rb
@@ -6,4 +6,13 @@ class LinkDate < ApplicationRecord
   validates :link_id, presence: true
 
   enum :date_type, { announcement: 0, release: 1, postponement: 2 }
+
+  def self.ransackable_associations(auth_object = nil)
+    ["link"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "date", "date_type", "id", "link_id", "remark", "updated_at"]
+  end
+
 end
