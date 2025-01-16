17453b309a41ef837ec7cd9d98e8b6a5382d766c
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-16 13:44:11 +0900

    fix: enum修正

--- app/models/link_view.rb
+++ app/models/link_view.rb
@@ -4,5 +4,5 @@ class LinkView < ApplicationRecord
   validates :date, presence: true
   validates :record_type, presence: true
 
-  enum :record_type, { one_million :0, ten_million :1, hundred_million :2, one_billion :3 }
+  enum :record_type, { one_million: 0, ten_million: 1, hundred_million: 2, one_billion: 3 }
 end
