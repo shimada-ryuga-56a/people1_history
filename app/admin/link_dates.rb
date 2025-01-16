27a9aad62aae80c67e49798521e98956ae915942
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-16 09:19:51 +0900

    fix: link_datesを管理画面タブに表示しないよう変更

--- app/admin/link_dates.rb
+++ app/admin/link_dates.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register LinkDate do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :date, :link_id, :date_type, :remark
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:date, :link_id, :date_type, :remark]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  menu false
 end
