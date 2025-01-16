353a2b5e9ede7f2a479a23695d686416dfc5eeaa
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-16 11:23:40 +0900

    add: LinkDate画像表示ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get '/history_events/:id' => "histories#event_image"
   get '/history_histories/:id' => "histories#history_image"
   get '/history_tie_ups/:id' => "histories#tie_up_image"
+  get '/history_link_dates/:id' => "histories#link_date_image"
   get '/histories/images/:id' => "histories#show_page_image"
 
   # （ここから）Twitter認証以外を認めないようにルーティングを設定しようとした痕跡
