f4d74de526f5579203a65d51523cc60107fba132
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-16 13:11:27 +0900

    add: linkとlink_viewの関連付け追加

---
+++ app/models/link_view.rb
@@ -0,0 +1,8 @@
+class LinkView < ApplicationRecord
+  belongs_to :link
+
+  validates :date, presence: true
+  validates :record_type, presence: true
+
+  enum :record_type, { one_million :0, ten_million :1, hundred_million :2, one_billion :3 }
+end
