bb5c1497a421d54de85e2e159b69f441d173efc1
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-16 10:47:51 +0900

    fix: リンク管理画面でタイトルの編集を許可

--- app/admin/links.rb
+++ app/admin/links.rb
@@ -3,13 +3,14 @@ ActiveAdmin.register Link do
   filter :updated_at
   filter :remark
 
-  permit_params :platform, :url_link, :remark, link_dates_attributes: [:id, :link_id, :date, :date_type, :remark, :_destroy]
+  permit_params :platform, :url_link, :remark, :title, link_dates_attributes: [:id, :link_id, :date, :date_type, :remark, :_destroy]
 
   form do |f|
     f.inputs do
       f.input :platform
       f.input :url_link
       f.input :remark
+      f.input :title
     end
 
     f.inputs do
