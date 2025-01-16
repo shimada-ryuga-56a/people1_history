586d91aa20cc1ced7a6d9824588eeed57e1a589f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-16 13:10:31 +0900

    add: link_viewsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_16_013907) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_16_040518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -222,6 +222,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_16_013907) do
     t.integer "link_id", null: false
     t.integer "date_type", null: false
     t.text "remark"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "link_views", force: :cascade do |t|
+    t.integer "link_id", null: false
+    t.date "date", null: false
+    t.integer "record_type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
