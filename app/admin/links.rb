03a582dd6b916c1238fd91283d4cb7dd0ea684ce
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-16 09:26:34 +0900

    fix: リンク管理画面からlink_datesを作成できるよう編集

--- app/admin/links.rb
+++ app/admin/links.rb
@@ -1,6 +1,25 @@
 ActiveAdmin.register Link do
-  permit_params :platform, :url_link, :remark
   filter :created_at
   filter :updated_at
   filter :remark
+
+  permit_params :platform, :url_link, :remark, link_dates_attributes: [:id, :link_id, :date, :date_type, :remark, :_destroy]
+
+  form do |f|
+    f.inputs do
+      f.input :platform
+      f.input :url_link
+      f.input :remark
+    end
+
+    f.inputs do
+      f.has_many :link_dates, allow_destroy: true, new_record: true do |t|
+        t.input :date
+        t.input :date_type
+        t.input :remark
+      end
+    end
+
+    f.actions
+  end
 end
--- app/models/link_date.rb
+++ app/models/link_date.rb
@@ -3,7 +3,6 @@ class LinkDate < ApplicationRecord
 
   validates :date, presence: true
   validates :date_type, presence: true
-  validates :link_id, presence: true
 
   enum :date_type, { announcement: 0, release: 1, postponement: 2 }
 
