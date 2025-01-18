5fca2d54fdf4ad95627faa511844bbd63252ece0
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-18 18:30:59 +0900

    add: 各種ルーティング追加

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,12 @@
 class MembersController < ApplicationController
   def index
+    @members = Member.all.order(:id)
+  end
+
+  def show
+  end
+
+  def image
+    @member = Member.find(params[:id])
   end
 end
