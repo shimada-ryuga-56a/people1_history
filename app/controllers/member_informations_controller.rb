a0e69a0166446a083a4bb1295d8b0361457137cb
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-19 23:35:16 +0900

    add: （仮）フォーム送信に関するパラメータ関連の記述

--- app/controllers/member_informations_controller.rb
+++ app/controllers/member_informations_controller.rb
@@ -1,4 +1,16 @@
 class MemberInformationsController < ApplicationController
   def create
+    @member_information = MemberInformation.new(member_params)
+    if @member_information.save
+      redirect_to member_path(@member_information.member)
+    else
+      redirect_to member_path(@member_information.member)
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member_information).permit(:content).merge(user_id: current_user.id, member_id: params[:member_id])
   end
 end
