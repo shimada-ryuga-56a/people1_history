e5e19dc5d134e0f29ebb2b5ec42f67a0f8aa97c2
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-19 11:33:06 +0900

    fix: enum値の修正

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -3,7 +3,7 @@ class Instrument < ApplicationRecord
   has_many :gears, dependent: :destroy
   accepts_nested_attributes_for :gears, allow_destroy: true
 
-  enum :name, { guitar: 0, bass: 1, drums: 2, keyboard: 3, vocal: 4, other: 5, trumpet: 6}
+  enum :name, { vocal: 0, bass: 1, drums: 2, keyboard: 3, guitar: 4, sampler: 5, trumpet: 6, other: 20}
 
   validates :name, presence: true
 end
