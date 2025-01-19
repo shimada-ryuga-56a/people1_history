832846fa29748a780f6e671af67dd0f603980486
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-19 12:56:37 +0900

    add: メンバーインフォテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_18_064425) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_19_035446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -256,6 +256,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_18_064425) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title", default: "title"
+  end
+
+  create_table "member_informations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "member_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "members", force: :cascade do |t|
