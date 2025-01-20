f8f204e921753f1b009d59b56608b7b35246be98
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-20 15:21:44 +0900

    add: いいねに関するアクション記述

--- app/controllers/likes_on_member_infos_controller.rb
+++ app/controllers/likes_on_member_infos_controller.rb
@@ -1,7 +1,13 @@
 class LikesOnMemberInfosController < ApplicationController
   def create
+    @info = MemberInformation.find(params[:member_information_id])
+    @like = @info.likes_on_member_infos.new(user_id: current_user.id)
+    @like.save
   end
 
   def destroy
+    @info = MemberInformation.find(params[:member_information_id])
+    @like = @info.likes_on_member_infos.find_by(user_id: current_user.id)
+    @like.destroy
   end
 end
