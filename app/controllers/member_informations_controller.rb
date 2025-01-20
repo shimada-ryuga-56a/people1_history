f55772cad8fbfe9bfc9c40507fa97d09dbf68b16
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-20 11:26:35 +0900

    add: メンバーインフォ投稿失敗時のアクション記述

--- app/controllers/member_informations_controller.rb
+++ app/controllers/member_informations_controller.rb
@@ -1,15 +1,17 @@
 class MemberInformationsController < ApplicationController
   def create
     @member_information = MemberInformation.new(member_params)
+    @member = Member.find(params[:member_id])
     respond_to do |format|
       if @member_information.save
         @new_info = MemberInformation.new
-        @member = Member.find(params[:member_id])
         @member_informations = @member.member_informations.includes(:user, :rich_text_content).order(created_at: :desc)
         flash.now[:success] = I18n.t('flash.success.post')
         format.turbo_stream { render 'members/turbo_stream/info_create' }
       else
-        redirect_to member_path(@member_information.member)
+        @new_info = @member_information
+        flash.now[:error] = I18n.t('flash.error.post')
+        format.turbo_stream { render 'members/turbo_stream/info_create_failure', status: :unprocessable_entity }
       end
     end
   end
