0fd70de5bfc2fdd6c2c79321317176763dd457e9
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-20 15:31:25 +0900

    add: 各アクションの非同期処理を記述

--- app/controllers/likes_on_member_infos_controller.rb
+++ app/controllers/likes_on_member_infos_controller.rb
@@ -3,11 +3,17 @@ class LikesOnMemberInfosController < ApplicationController
     @info = MemberInformation.find(params[:member_information_id])
     @like = @info.likes_on_member_infos.new(user_id: current_user.id)
     @like.save
+    respond_to do |format|
+      format.turbo_stream { render 'members/likes_on_member_info/like' }
+    end
   end
 
   def destroy
     @info = MemberInformation.find(params[:member_information_id])
     @like = @info.likes_on_member_infos.find_by(user_id: current_user.id)
     @like.destroy
+    respond_to do |format|
+      format.turbo_stream { render 'members/likes_on_member_info/unlike' }
+    end
   end
 end
