3a67dae1e29a4fe15b76f4d799e0b6e6bef5aec0
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-23 16:22:27 +0900

    fix: イベントインフォの更新処理を記述

--- app/controllers/event_informations_controller.rb
+++ app/controllers/event_informations_controller.rb
@@ -1,14 +1,14 @@
 class EventInformationsController < ApplicationController
   def create
     @info = EventInformation.new(event_information_params)
+    @event = Event.find(params[:event_id])
     respond_to do |format|
       if @info.save
+        @infos = EventInformation.eager_load(:user, :likes_on_event_informations).where(event_id: @event.id).order(created_at: :desc)
         @new_info = EventInformation.new
-        @event = Event.find(params[:event_id])
         flash.now[:success] = I18n.t('flash.success.post')
         format.turbo_stream { render 'create' }
       else
-        @event = Event.includes(:setlist).find(params[:event_id])
         @new_info = @info
         flash.now[:error] = I18n.t('flash.error.post')
         format.turbo_stream { render 'create_failure', status: :unprocessable_entity }
