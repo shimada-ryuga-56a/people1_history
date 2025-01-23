8ccce2dd3b0a2cce83c8eae62fb7d22194b267a8
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-23 15:40:07 +0900

    fix: いいねの部分更新を実装

--- app/controllers/likes_on_event_informations_controller.rb
+++ app/controllers/likes_on_event_informations_controller.rb
@@ -3,7 +3,9 @@ class LikesOnEventInformationsController < ApplicationController
     event_information = EventInformation.find(params[:event_information_id])
     likes_on_event_information = current_user.likes_on_event_informations.new(event_information_id: event_information.id)
     likes_on_event_information.save
-    redirect_to event_path(id: event_information.event.id)
+    # respond_to do |format|
+    #   format.turbo_stream { render '', locals: {  } }
+    # end
   end
 
   def destroy
--- app/controllers/likes_on_setlistitem_informations_controller.rb
+++ app/controllers/likes_on_setlistitem_informations_controller.rb
@@ -4,7 +4,9 @@ class LikesOnSetlistitemInformationsController < ApplicationController
     likes_on_setlistitem_information =
       current_user.likes_on_setlistitem_informations.new(setlistitem_information_id: setlistitem_information.id)
     likes_on_setlistitem_information.save
-    redirect_to event_path(id: setlistitem_information.setlistitem.setlist.event.id)
+    respond_to do |format|
+      format.turbo_stream { render 'setlistitem_informations/like', locals: { setlistitem_info: setlistitem_information } }
+    end
   end
 
   def destroy
@@ -12,6 +14,8 @@ class LikesOnSetlistitemInformationsController < ApplicationController
     likes_on_setlistitem_information =
       current_user.likes_on_setlistitem_informations.find_by(setlistitem_information_id: setlistitem_information.id)
     likes_on_setlistitem_information.destroy
-    redirect_to event_path(id: setlistitem_information.setlistitem.setlist.event.id)
+    respond_to do |format|
+      format.turbo_stream { render 'setlistitem_informations/like', locals: { setlistitem_info: setlistitem_information } }
+    end
   end
 end
