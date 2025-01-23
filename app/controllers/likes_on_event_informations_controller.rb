ffb2cd6d017a2493e71d2fbbe285918a6ea55de4
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-01-23 16:29:39 +0900

    fix: いいねの更新を修正

--- app/controllers/likes_on_event_informations_controller.rb
+++ app/controllers/likes_on_event_informations_controller.rb
@@ -3,15 +3,17 @@ class LikesOnEventInformationsController < ApplicationController
     event_information = EventInformation.find(params[:event_information_id])
     likes_on_event_information = current_user.likes_on_event_informations.new(event_information_id: event_information.id)
     likes_on_event_information.save
-    # respond_to do |format|
-    #   format.turbo_stream { render '', locals: {  } }
-    # end
+    respond_to do |format|
+      format.turbo_stream { render 'event_informations/like', locals: { event_information: event_information } }
+    end
   end
 
   def destroy
     event_information = EventInformation.find(params[:event_information_id])
     likes_on_event_information = current_user.likes_on_event_informations.find_by(event_information_id: event_information.id)
     likes_on_event_information.destroy
-    redirect_to event_path(id: event_information.event.id)
+    respond_to do |format|
+      format.turbo_stream { render 'event_informations/unlike', locals: { event_information: event_information } }
+    end
   end
 end
