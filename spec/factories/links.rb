82aa40ce60a80e3c9b45220456d77247ed233531
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-01 14:46:41 +0900

    fix: リンクのプレフィックスを複数用意

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :link do
     texts = ("a".."z").to_a
+    link_prefixes = %w(https://www.youtube.com/watch?v= https://www.tiktok.com/)
 
     platform {"YouTube"}
     sequence(:url_link) { |n|
-      "https://www.youtube.com/watch?v=" + texts.sample(2 + (n % 4)).join
+      link_prefixes.sample + texts.sample(2 + (n % 4)).join
     }
     sequence(:remark) { |n|
       texts.sample(2 + (n % 4)).join
