6ca7daf2475b91a2a2b87b63a0efae93bedba36c
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-01 14:41:36 +0900

    add: songモデルスペック追加 （リンクとの紐付けテスト）

---
+++ spec/models/song_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Song, type: :model do
+  describe 'バリデーションチェック' do
+    it "設定したすべてのバリデーションが機能しているか" do
+      song = create(:song)
+      expect(song).to be_valid
+      expect(song.errors).to be_empty
+    end
+    it "複数のリンクと紐付けられるか" do
+      song = create(:song)
+      link = create(:link)
+      link_content = create(:link_content, linkable_id: song.id, linkable_type: "Song", link_id: link.id)
+      expect(link_content).to be_valid
+      expect(link_content.errors).to be_empty
+    end
+  end
+end
