d5181e4b225abe1b80ad8e30f7b102c72e2a3cc5
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-01 14:15:44 +0900

    fix: FactoryBotの省略形が使えるように修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,6 +43,7 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
@@ -67,4 +68,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
 end
