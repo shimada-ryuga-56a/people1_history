6224090136a63fb4725ac353b4a8afef6c1b291e
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-01 13:25:09 +0900

    fix: FactoryBotが二重で使用されてしまうため、コメントアウト

--- test/factories/songs.rb
+++ test/factories/songs.rb
@@ -1,8 +1,8 @@
-FactoryBot.define do
-  factory :song do
-    id { 1 }
-    name { 'MyText' }
-    name_kana_ruby { 'テスト' }
-    name_hiragana_ruby { 'てすと' }
-  end
-end
+# FactoryBot.define do
+#   factory :song do
+#     id { 1 }
+#     name { 'MyText' }
+#     name_kana_ruby { 'テスト' }
+#     name_hiragana_ruby { 'てすと' }
+#   end
+# end
