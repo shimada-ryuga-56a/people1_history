0fb4dcb6bcbde4d25067027d79cc7fce6f7b0b68
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-02 14:35:33 +0900

    add: OGP生成アクション追加

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,22 @@
 class ImagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:ogp]
+
   def history_image_destroy
     @history = current_user.histories.find(params[:history_id])
     @image = @history.image
     @image.purge
     redirect_to edit_history_path(@history)
+  end
+
+  def ogp
+    text = ogp_params[:text]
+    image = OgpCreator.build(text).tempfile.open.read
+    send_data image, type: 'image/png', disposition: 'inline'
+  end
+
+  private
+
+  def ogp_params
+    params.permit(:text)
   end
 end
