9c0b9849baa38977754b4bb776143baea307d056
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-02 14:35:59 +0900

    fix: OGP画像微修正

--- app/controllers/concerns/ogp_creator.rb
+++ app/controllers/concerns/ogp_creator.rb
@@ -1,10 +1,10 @@
 class OgpCreator
   require 'mini_magick'
-  BASE_IMAGE_PATH = './app/assets/images/{dynamic_ogp_background.jpng}'
-  GRAVITY = 'center'
+  BASE_IMAGE_PATH = './app/assets/images/dynamic_ogp_background.jpeg'
+  GRAVITY = 'Center'
   TEXT_POSITION = '0,0'
-  FONT = './app/assets/fonts/DotGothic16'
-  FONT_SIZE = 65
+  FONT = './app/assets/fonts/DotGothic16,M_PLUS_1p/DotGothic16/DotGothic16-Regular.ttf'
+  FONT_SIZE = 50
   INDENTION_COUNT = 16
   ROW_LIMIT = 8
 
@@ -13,7 +13,7 @@ class OgpCreator
     image = MiniMagick::Image.open(BASE_IMAGE_PATH)
     image.combine_options do |config|
       config.font FONT
-      config.fill 'red'
+      config.fill 'white'
       config.gravity GRAVITY
       config.pointsize FONT_SIZE
       config.draw "text #{TEXT_POSITION} '#{text}'"
