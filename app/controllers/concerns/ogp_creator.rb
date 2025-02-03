99da2d2ca52774fbdd12e551e2ab2f8febfbf55f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-03 11:19:14 +0900

    fix: 渡された文字列を表示 （長いイベント名だとはみ出してしまうため今後修正する）

--- app/controllers/concerns/ogp_creator.rb
+++ app/controllers/concerns/ogp_creator.rb
@@ -1,22 +1,35 @@
 class OgpCreator
   require 'mini_magick'
   BASE_IMAGE_PATH = './app/assets/images/dynamic_ogp_background.jpeg'
-  GRAVITY = 'Center'
-  TEXT_POSITION = '0,0'
+  GRAVITY = 'NorthWest'
+  TEXT_POSITION = '+70+97'
+  TEXT_SHADOW_POSITION = '+68+100'
+  SUB_TEXT_POSITION = '+70+60'
+  SUB_TEXT_SHADOW_POSITION = '+68+63'
   FONT = './app/assets/fonts/DotGothic16,M_PLUS_1p/DotGothic16/DotGothic16-Regular.ttf'
-  FONT_SIZE = 50
-  INDENTION_COUNT = 16
-  ROW_LIMIT = 8
+  FONT_SIZE = 40
+  SUB_FONT_SIZE = 20
+  INDENTION_COUNT = 22
+  ROW_LIMIT = 2
 
-  def self.build(text)
+  def self.build(text, sub_text)
     text = prepare_text(text)
+    sub_text = prepare_text(sub_text)
     image = MiniMagick::Image.open(BASE_IMAGE_PATH)
     image.combine_options do |config|
       config.font FONT
-      config.fill 'white'
       config.gravity GRAVITY
+      config.fill 'black'
+      config.pointsize FONT_SIZE
+      config.draw "text #{TEXT_SHADOW_POSITION} '#{text}'"
+      config.fill 'white'
       config.pointsize FONT_SIZE
       config.draw "text #{TEXT_POSITION} '#{text}'"
+      config.pointsize SUB_FONT_SIZE
+      config.fill 'black'
+      config.draw "text #{SUB_TEXT_SHADOW_POSITION} '#{sub_text}'"
+      config.fill 'white'
+      config.draw "text #{SUB_TEXT_POSITION} '#{sub_text}'"
     end
   end
 
