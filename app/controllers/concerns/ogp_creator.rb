49c80738a54100577c32ea74d2e21395214d2824
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-03 12:00:36 +0900

    fix: レイアウト修正

--- app/controllers/concerns/ogp_creator.rb
+++ app/controllers/concerns/ogp_creator.rb
@@ -1,18 +1,22 @@
 class OgpCreator
   require 'mini_magick'
   BASE_IMAGE_PATH = './app/assets/images/dynamic_ogp_background.jpeg'
+  FONT = './app/assets/fonts/DotGothic16,M_PLUS_1p/DotGothic16/DotGothic16-Regular.ttf'
   GRAVITY = 'NorthWest'
+  LONG_TEXT_FONT_SIZE = 25
+  NORMAL_FONT_SIZE = 35
   TEXT_POSITION = '+70+97'
   TEXT_SHADOW_POSITION = '+68+100'
+  SUB_FONT_SIZE = 15
   SUB_TEXT_POSITION = '+70+60'
   SUB_TEXT_SHADOW_POSITION = '+68+63'
-  FONT = './app/assets/fonts/DotGothic16,M_PLUS_1p/DotGothic16/DotGothic16-Regular.ttf'
-  FONT_SIZE = 40
-  SUB_FONT_SIZE = 20
-  INDENTION_COUNT = 22
+  REMARK_FONT_SIZE = 20
+  REMARK_POSITION = '+70+190'
+  REMARK_SHADOW_POSITION = '+68+193'
+  INDENTION_COUNT = 34
   ROW_LIMIT = 2
 
-  def self.build(text, sub_text)
+  def self.build(text, sub_text, remark)
     text = prepare_text(text)
     sub_text = prepare_text(sub_text)
     image = MiniMagick::Image.open(BASE_IMAGE_PATH)
@@ -20,16 +24,29 @@ class OgpCreator
       config.font FONT
       config.gravity GRAVITY
       config.fill 'black'
-      config.pointsize FONT_SIZE
+      if text.length < 28
+        config.pointsize NORMAL_FONT_SIZE
+      else
+        config.pointsize LONG_TEXT_FONT_SIZE
+      end
       config.draw "text #{TEXT_SHADOW_POSITION} '#{text}'"
       config.fill 'white'
-      config.pointsize FONT_SIZE
+      if text.length < 28
+        config.pointsize NORMAL_FONT_SIZE
+      else
+        config.pointsize LONG_TEXT_FONT_SIZE
+      end
       config.draw "text #{TEXT_POSITION} '#{text}'"
       config.pointsize SUB_FONT_SIZE
       config.fill 'black'
       config.draw "text #{SUB_TEXT_SHADOW_POSITION} '#{sub_text}'"
       config.fill 'white'
       config.draw "text #{SUB_TEXT_POSITION} '#{sub_text}'"
+      config.pointsize REMARK_FONT_SIZE
+      config.fill 'black'
+      config.draw "text #{REMARK_SHADOW_POSITION} '#{remark}'"
+      config.fill 'white'
+      config.draw "text #{REMARK_POSITION} '#{remark}'"
     end
   end
 
