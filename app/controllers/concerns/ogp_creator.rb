fc00f49cd858bbc664b5e9b880fbcc449f9f068f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-04 21:30:13 +0900

    fix: マジックナンバーの定数化

--- app/controllers/concerns/ogp_creator.rb
+++ app/controllers/concerns/ogp_creator.rb
@@ -3,17 +3,19 @@ class OgpCreator
   BASE_IMAGE_PATH = './app/assets/images/dynamic_ogp_background.jpeg'
   FONT = './app/assets/fonts/DotGothic16,M_PLUS_1p/DotGothic16/DotGothic16-Regular.ttf'
   GRAVITY = 'NorthWest'
+  MINI_TEXT_MAX_LENGTH = 26
   LONG_TEXT_FONT_SIZE = 25
   NORMAL_FONT_SIZE = 35
   TEXT_POSITION = '+70+97'
-  TEXT_SHADOW_POSITION = '+68+100'
+  TEXT_SHADOW_POSITION = '+68+98'
   SUB_FONT_SIZE = 15
   SUB_TEXT_POSITION = '+70+60'
-  SUB_TEXT_SHADOW_POSITION = '+68+63'
+  SUB_TEXT_SHADOW_POSITION = '+68+61'
   REMARK_FONT_SIZE = 20
   REMARK_POSITION = '+70+190'
   REMARK_SHADOW_POSITION = '+68+193'
   INDENTION_COUNT = 34
+  SUB_TEXT_INDENTION_COUNT = 34
   ROW_LIMIT = 2
 
   def self.build(text, sub_text, remark)
@@ -24,14 +26,14 @@ class OgpCreator
       config.font FONT
       config.gravity GRAVITY
       config.fill 'black'
-      if text.length < 28
+      if text.bytesize < MINI_TEXT_MAX_LENGTH
         config.pointsize NORMAL_FONT_SIZE
       else
         config.pointsize LONG_TEXT_FONT_SIZE
       end
       config.draw "text #{TEXT_SHADOW_POSITION} '#{text}'"
       config.fill 'white'
-      if text.length < 28
+      if text.bytesize < MINI_TEXT_MAX_LENGTH
         config.pointsize NORMAL_FONT_SIZE
       else
         config.pointsize LONG_TEXT_FONT_SIZE
