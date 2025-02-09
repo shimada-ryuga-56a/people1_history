24a868a53e0ca063adc34e436b1e96a14bd5268b
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-09 23:52:32 +0900

    fix: 各種バリデーションを満たす・満たさないレコード生成を記述

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
-  factory :song do
-    hiragana_texts = ('あ'..'ん').to_a
-    katakana_texts = ('ア'..'ン').to_a
+  hiragana_texts = ('あ'..'ん').to_a
+  katakana_texts = ('ア'..'ン').to_a
 
+  factory :song do
     sequence(:name) { |n| "title_#{n}" }
     sequence(:name_hiragana_ruby) do |n|
       hiragana_texts.sample(2 + (n % 4)).join
@@ -10,5 +10,54 @@ FactoryBot.define do
     sequence(:name_kana_ruby) do |n|
       katakana_texts.sample(2 + (n % 4)).join
     end
+  end
+
+  factory :correct_song, class: Song do
+    sequence(:name) { |n| "title_#{n}" }
+    sequence(:name_hiragana_ruby) do |n|
+      hiragana_texts.sample(2 + (n % 4)).join
+    end
+    sequence(:name_kana_ruby) do |n|
+      katakana_texts.sample(2 + (n % 4)).join
+    end
+  end
+
+  factory :no_name_song, class: Song do
+    sequence(:name_hiragana_ruby) do |n|
+      hiragana_texts.sample(2 + (n % 4)).join
+    end
+    sequence(:name_kana_ruby) do |n|
+      katakana_texts.sample(2 + (n % 4)).join
+    end
+  end
+
+  factory :no_name_hiragana_ruby_song, class: Song do
+    sequence(:name) { |n| "title_#{n}" }
+    sequence(:name_kana_ruby) do |n|
+      katakana_texts.sample(2 + (n % 4)).join
+    end
+  end
+
+  factory :incorrect_name_hiragana_ruby_song, class: Song do
+    sequence(:name) { |n| "title_#{n}" }
+    name_hiragana_ruby { 'アイウエオ' }
+    sequence(:name_kana_ruby) do |n|
+      katakana_texts.sample(2 + (n % 4)).join
+    end
+  end
+
+  factory :no_name_kana_ruby_song, class: Song do
+    sequence(:name) { |n| "title_#{n}" }
+    sequence(:name_hiragana_ruby) do |n|
+      hiragana_texts.sample(2 + (n % 4)).join
+    end
+  end
+
+  factory :incorrect_name_kana_ruby_song, class: Song do
+    sequence(:name) { |n| "title_#{n}" }
+    sequence(:name_hiragana_ruby) do |n|
+      hiragana_texts.sample(2 + (n % 4)).join
+    end
+    name_kana_ruby { 'あいうえお' }
   end
 end
