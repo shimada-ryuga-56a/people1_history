8652d5649e61b45557cf5703cbb3a64e380e49e1
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-09 23:56:30 +0900

    improve: 属性を満たす場合のテストの書き方を変更

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -7,18 +7,12 @@ RSpec.describe Song, type: :model do
   let (:incorrect_name_hiragana_ruby_song) { build(:incorrect_name_hiragana_ruby_song) }
   let (:no_name_kana_ruby_song) { build(:no_name_kana_ruby_song) }
   let (:incorrect_name_kana_ruby_song) { build(:incorrect_name_kana_ruby_song) }
-  describe 'バリデーションチェック' do
-    it '設定したすべてのバリデーションが機能しているか' do
-      song = create(:song)
-      expect(song).to be_valid
-      expect(song.errors).to be_empty
-    end
-    it '複数のリンクと紐付けられるか' do
-      song = create(:song)
-      link = create(:link)
-      link_content = create(:link_content, linkable_id: song.id, linkable_type: 'Song', link_id: link.id)
-      expect(link_content).to be_valid
-      expect(link_content.errors).to be_empty
+  describe '[Model : Song] Success - バリデーションの確認' do
+    context '全ての属性が正しい場合' do
+      it 'エラーなくSongが作成される' do
+        expect(correct_song).to be_valid
+        expect(correct_song.errors).to be_empty
+      end
     end
   end
 end
