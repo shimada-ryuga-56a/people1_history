34d8f94c5d84ff9627aa6ca72fd0a0e77a9a2ad4
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-10 00:03:12 +0900

    fix: バリデーションエラーのテスト実装

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -15,4 +15,41 @@ RSpec.describe Song, type: :model do
       end
     end
   end
+
+  describe '[Model : Song] Failure - バリデーションの確認' do
+    context 'nameが空の場合' do
+      it 'エラーが発生する' do
+        expect(no_name_song).to be_invalid
+        expect(no_name_song.errors.messages[:name]).to include "を入力してください"
+      end
+    end
+
+    context 'name_hiragana_rubyが空の場合' do
+      it 'エラーが発生する' do
+        expect(no_name_hiragana_ruby_song).to be_invalid
+        expect(no_name_hiragana_ruby_song.errors.messages[:name_hiragana_ruby]).to include "を入力してください"
+      end
+    end
+
+    context 'name_hiragana_rubyがひらがなではない場合' do
+      it 'エラーが発生する' do
+        expect(incorrect_name_hiragana_ruby_song).to be_invalid
+        expect(incorrect_name_hiragana_ruby_song.errors.messages[:name_hiragana_ruby]).to include '全角ひらがなで入力して下さい。'
+      end
+    end
+
+    context 'name_kana_rubyが空の場合' do
+      it 'エラーが発生する' do
+        expect(no_name_kana_ruby_song).to be_invalid
+        expect(no_name_kana_ruby_song.errors.messages[:name_kana_ruby]).to include "を入力してください"
+      end
+    end
+
+    context 'name_kana_rubyが全角カタカナ以外の場合' do
+      it 'エラーが発生する' do
+        expect(incorrect_name_kana_ruby_song).to be_invalid
+        expect(incorrect_name_kana_ruby_song.errors.messages[:name_kana_ruby]).to include "全角カタカナで入力して下さい。"
+      end
+    end
+  end
 end
