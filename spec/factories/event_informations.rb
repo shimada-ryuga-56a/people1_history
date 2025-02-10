94e45600ed17a6193fb0f728421e88107276c560
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-10 20:14:48 +0900

    fix: event_info FactoryBot アソシエーション用のコード記述し直し

--- spec/factories/event_informations.rb
+++ spec/factories/event_informations.rb
@@ -4,20 +4,21 @@ FactoryBot.define do
     association :event
     user_id { user.id }
     event_id { event.id }
+    sequence(:body) { |_n| 'body' }
+  end
 
-    factory :correct_event_information, class: EventInformation do
-      association :user
-      association :event
-      user_id { user.id }
-      event_id { event.id }
-      sequence(:body) { |_n| 'body' }
-    end
+  factory :correct_event_information, class: EventInformation do
+    association :user
+    association :event
+    user_id { user.id }
+    event_id { event.id }
+    sequence(:body) { |_n| 'body' }
+  end
 
-    factory :no_body_event_information, class: EventInformation do
-      association :user
-      association :event
-      user_id { user.id }
-      event_id { event.id }
-    end
+  factory :no_body_event_information, class: EventInformation do
+    association :user
+    association :event
+    user_id { user.id }
+    event_id { event.id }
   end
 end
