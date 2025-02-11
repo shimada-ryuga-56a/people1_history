05c4f928908ef3813797d839754b478d17cfd228
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-11 15:55:05 +0900

    add: セットリスト 最低一曲を要求するバリデーション設置

--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -4,4 +4,12 @@ class Setlist < ApplicationRecord
   has_many :setlistitems, dependent: :destroy
 
   accepts_nested_attributes_for :setlistitems, allow_destroy: true, reject_if: :all_blank
+
+  validate :at_least_one_setlistitem
+
+  def at_least_one_setlistitem
+    if setlistitems.reject { |item| item.song_title.blank? }.empty?
+      errors.add(:setlistitems, 'は1つ以上必要です')
+    end
+  end
 end
