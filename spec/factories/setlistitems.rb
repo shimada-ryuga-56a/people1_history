d6324d0925a62158734d85f01898204dc7ea0549
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-11 23:42:32 +0900

    add: setlistitem FactoryBot作成

--- spec/factories/setlistitems.rb
+++ spec/factories/setlistitems.rb
@@ -1,5 +1,24 @@
 FactoryBot.define do
   factory :setlistitem do
-    
+    song_title { 'song_title' }
+  end
+
+  factory :correct_setlistitem, class: Setlistitem do
+    song_title { 'song_title' }
+  end
+
+  factory :rehearsal_setlistitem, class; Setlistitem do
+    song_title { 'song_title' }
+    is_rehearsal { true }
+  end
+
+  factory :encore_setlistitem, class: Setlistitem do
+    song_title { 'song_title' }
+    is_encore { true }
+  end
+
+  factory :not_song_setlistitem, class: Setlistitem do
+    song_title { 'song_title' }
+    is_song { false }
   end
 end
