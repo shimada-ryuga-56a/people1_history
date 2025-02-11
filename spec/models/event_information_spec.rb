d2505e8399db22479e5373668b2d7e5e6acba487
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-11 14:54:05 +0900

    add: event_infoアソシエーションテスト記述

--- spec/models/event_information_spec.rb
+++ spec/models/event_information_spec.rb
@@ -3,11 +3,21 @@ require 'rails_helper'
 RSpec.describe EventInformation, type: :model do
   let (:correct_event_information) { build(:correct_event_information) }
   let (:no_body_event_information) { build(:no_body_event_information) }
+  before do
+    @user = FactoryBot.create(:user)
+  end
   describe '正常系: バリデーション' do
     context '全ての属性が正しい場合' do
       it 'エラーなくEventInformationが作成される' do
         expect(correct_event_information).to be_valid
         expect(correct_event_information.errors).to be_empty
+      end
+    end
+
+    context '正しくEventInformationが作成された場合' do
+      it 'Userとの紐付けが正しく保存されること' do
+        FactoryBot.create(:correct_event_information, user: @user)
+        expect(correct_event_information.user).to eq @user
       end
     end
   end
