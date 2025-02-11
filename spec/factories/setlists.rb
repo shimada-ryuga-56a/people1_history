2d9e0de587c525f06f85e9f44f573e6a8c5a748b
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-11 23:28:19 +0900

    add: Setlist FactoryBot記述

--- spec/factories/setlists.rb
+++ spec/factories/setlists.rb
@@ -1,5 +1,25 @@
 FactoryBot.define do
   factory :setlist do
-    
+    association :event
+    association :user
+    user_id { user.id }
+    event_id { event.id }
+  end
+
+  factory :correct_setlist, class: Setlist do
+    association :event
+    association :user
+    user_id { user.id }
+    event_id { event.id }
+  end
+
+  factory :no_event_setlist, class: Setlist do
+    association :user
+    user_id { user.id }
+  end
+
+  factory :no_user_setlist, class: Setlist do
+    association :event
+    event_id { event.id }
   end
 end
