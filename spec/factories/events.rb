15a765182d0050c1b50837ecc2a24058e2b89fe4
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-11 17:43:53 +0900

    add: event FactoryBot作成

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,45 @@
 FactoryBot.define do
   factory :event do
-    
+    category { Event.categories.keys.sample }
+    name { "イベント名" }
+    date { Faker::Date.between(from: 2.days.ago, to: Date.today) }
+    name_kana_ruby { "イベントメイ" }
+    is_canceled { false }
+    place { "イベント会場" }
+    place_prefecture { Event.place_prefectures.keys.sample }
+    remark { "備考文" }
+    # if category == "Tour"
+    #   association :tour, factory: :tour
+    # end
+
+    after(:build) do |event|
+      event.visual_image.attach(
+        io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'test_image.png')),
+        filename: 'test_image.png',
+        content_type: 'image/png'
+      )
+    end
+  end
+
+  factory :correct_event do
+    category { Event.categories.keys.sample }
+    name { "イベント名" }
+    date { Faker::Date.between(from: 2.days.ago, to: Date.today) }
+    name_kana_ruby { "イベントメイ" }
+    is_canceled { false }
+    place { "イベント会場" }
+    place_prefecture { Event.place_prefectures.keys.sample }
+    remark { "備考文" }
+    # if category == "Tour"
+    #   association :tour, factory: :tour
+    # end
+
+    after(:build) do |event|
+      event.visual_image.attach(
+        io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'test_image.png')),
+        filename: 'test_image.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end
