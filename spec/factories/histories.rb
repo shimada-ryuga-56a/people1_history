186847fcd50c464c563e83e84ed3c2a6eda47c28
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-11 14:54:32 +0900

    add: history テストオブジェクト生成コード記述

--- spec/factories/histories.rb
+++ spec/factories/histories.rb
@@ -1,5 +1,31 @@
 FactoryBot.define do
   factory :history do
-    
+    association :user
+    user_id { user.id }
+    title { 'title' }
+    date { '2021-01-01' }
+    remark { 'remark' }
+  end
+
+  factory :correct_history, class: History do
+    association :user
+    user_id { user.id }
+    title { 'title' }
+    date { '2021-01-01' }
+    remark { 'remark' }
+  end
+
+  factory :no_title_history, class: History do
+    association :user
+    user_id { user.id }
+    date { '2021-01-01' }
+    remark { 'remark' }
+  end
+
+  factory :no_date_history, class: History do
+    association :user
+    user_id { user.id }
+    title { 'title' }
+    remark { 'remark' }
   end
 end
