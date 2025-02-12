6263461d97b9f619822562b56fc2a492c40850b6
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-12 23:34:48 +0900

    remove: バリデーションテストで使用を想定していた不要な記述を削除

--- spec/factories/setlists.rb
+++ spec/factories/setlists.rb
@@ -25,14 +25,4 @@ FactoryBot.define do
     user_id { user.id }
     event_id { event.id }
   end
-
-  factory :no_event_setlist, class: Setlist do
-    association :user
-    user_id { user.id }
-  end
-
-  factory :no_user_setlist, class: Setlist do
-    association :event
-    event_id { event.id }
-  end
 end
