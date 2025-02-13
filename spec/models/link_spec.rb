d1a5f2c1af7c90453eaf86195f2c8a234cb2b699
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-13 22:14:47 +0900

    fix: link関連モデルスペック 異常系テスト追加

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  describe 'バリデーションチェック' do
-    it '設定したすべてのバリデーションが機能しているか' do
-      link = create(:link)
-      expect(link).to be_valid
-      expect(link.errors).to be_empty
+  describe '正常系: バリデーション' do
+    context 'すべての属性が正しく設定されている場合' do
+      it 'リンクが作成される' do
+        link = create(:link)
+        expect(link).to be_valid
+        expect(link.errors).to be_empty
+      end
     end
+  end
+
+  describe '異常系: バリデーション' do
+    it { is_expected.to validate_presence_of :platform }
+    it { is_expected.to validate_presence_of :url_link }
+    it { is_expected.to validate_presence_of :remark }
   end
 end
