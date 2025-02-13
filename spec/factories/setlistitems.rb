68fd67b9500e51dc3051db1f8d8a6501724f1bf0
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-13 11:19:32 +0900

    fix: 各種setlistitemがsetlistと紐づくように修正

--- spec/factories/setlistitems.rb
+++ spec/factories/setlistitems.rb
@@ -33,16 +33,22 @@ FactoryBot.define do
   end
 
   factory :rehearsal_setlistitem, class: Setlistitem do
+    association :setlist
+    setlist_id { setlist.id }
     song_title { 'song_title' }
     is_rehearsal { true }
   end
 
   factory :encore_setlistitem, class: Setlistitem do
+    association :setlist
+    setlist_id { setlist.id }
     song_title { 'song_title' }
     is_encore { true }
   end
 
   factory :not_song_setlistitem, class: Setlistitem do
+    association :setlist
+    setlist_id { setlist.id }
     song_title { 'song_title' }
     is_song { false }
   end
