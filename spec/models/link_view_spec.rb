e3b6c203bdb77113c8f24aaf7ebac1337b9ef277
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-14 14:29:34 +0900

    add: link_view モデルスペック記述

--- spec/models/link_view_spec.rb
+++ spec/models/link_view_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe LinkView, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:link_view) { build(:link_view) }
+  let (:correct_link_view) { build(:correct_link_view) }
+
+  describe '正常系: バリデーション' do
+    context '全ての属性が正しい場合' do
+      it 'エラーなくLinkViewが作成される' do
+        expect(correct_link_view).to be_valid
+        expect(correct_link_view.errors).to be_empty
+      end
+    end
+  end
+
+  describe '異常系: バリデーション' do
+    it { is_expected.to validate_presence_of :date }
+    it { is_expected.to validate_presence_of :record_type }
+  end
 end
