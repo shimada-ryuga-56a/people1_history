7b58fdb66f7b1b15c04de20d5871d5c3352dfd56
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-14 14:28:44 +0900

    add: link_view FactoryBot記述

--- spec/factories/link_views.rb
+++ spec/factories/link_views.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :link_view do
-    
+    association :link
+    date { Date.today }
+    link_id { link.id }
+    record_type { :one_million }
+  end
+
+  factory :correct_link_view, class: LinkView do
+    association :link
+    date { Date.today }
+    link_id { link.id }
+    record_type { :one_million }
   end
 end
