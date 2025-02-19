fef204cc8781c54cb2703d8479af9ebf2a026f09
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-19 23:16:37 +0900

    fix: textsとlink_prefixesを全ての箇所で使えるように移行

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
-  factory :link do
-    texts = ('a'..'z').to_a
-    link_prefixes = %w[https://www.youtube.com/watch?v= https://www.tiktok.com/]
+  texts = ('a'..'z').to_a
+  link_prefixes = %w[https://www.youtube.com/watch?v= https://www.tiktok.com/]
 
+  factory :link do
     platform { 'YouTube' }
     sequence(:url_link) do |n|
       link_prefixes.sample + texts.sample(2 + (n % 4)).join
