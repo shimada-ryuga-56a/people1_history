3115af9216256ea18baffb8c6572953075686295
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-19 18:33:54 +0900

    add: youtube 各動画再生数取得ジョブ作成

--- app/jobs/fetch_youtube_record_job.rb
+++ app/jobs/fetch_youtube_record_job.rb
@@ -2,6 +2,13 @@ class FetchYoutubeRecordJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    require 'google/apis/youtube_v3'
+    @links = Link.where(platform: 0)
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = ENV['YOUTUBE_API_KEY']
+    @links.each do |link|
+      @response = service.list_videos('statistics', id: link.extract_youtube_id)
+      p @response.items.first.statistics.view_count
+    end
   end
 end
