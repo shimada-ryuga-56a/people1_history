45313de6e6465702a8b21aaab3a2003005a73c97
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-19 23:19:56 +0900

    add: youtubeとtiktokのリンクを作成

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -14,4 +14,18 @@ FactoryBot.define do
       texts.sample(2 + (n % 4)).join
     end
   end
+
+  factory :link_youtube, class: Link do
+    url_link { link_prefixes[0] + texts.sample(2).join }
+    platform { 'YouTube' }
+    remark { texts.sample(2).join }
+    title { texts.sample(2).join }
+  end
+
+  factory :link_tiktok, class: Link do
+    url_link { link_prefixes[1] + texts.sample(2).join }
+    platform { 'TikTok' }
+    remark { texts.sample(2).join }
+    title { texts.sample(2).join }
+  end
 end
