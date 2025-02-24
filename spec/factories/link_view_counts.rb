7410335d31b1b47cd531e4d79e50975e462a51d8
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-24 21:55:20 +0900

    add: link_view_countsのFactoryBotを作成

--- spec/factories/link_view_counts.rb
+++ spec/factories/link_view_counts.rb
@@ -1,7 +1,36 @@
 FactoryBot.define do
   factory :link_view_count do
-    link_id { 1 }
-    date { '2025-02-20' }
-    view_count { 1 }
+    association :link
+    date { Time.zone.today }
+    view_count { 1_000_000 }
+    link_id { link.id }
+  end
+
+  factory :correct_link_view_count, class: LinkViewCount do
+    association :link
+    date { Time.zone.today }
+    view_count { 1_000_000 }
+    link_id { link.id }
+  end
+
+  factory :previous_link_view_count, class: LinkViewCount do
+    association :link
+    date { 20_250_201 }
+    view_count { 1_000_000 }
+    link_id { link.id }
+  end
+
+  factory :now_same_million_link_view_count, class: LinkViewCount do
+    association :link
+    date { 20_250_202 }
+    view_count { 1_500_000 }
+    link_id { link.id }
+  end
+
+  factory :next_million_link_view_count, class: LinkViewCount do
+    association :link
+    date { 20_250_203 }
+    view_count { 2_000_000 }
+    link_id { link.id }
   end
 end
