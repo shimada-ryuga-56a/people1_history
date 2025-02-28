787dee9f76d6f9c4d3a741a9b93de678c0160d4a
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-02-28 14:47:02 +0900

    add: 定期実行のための環境変数明記

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/environment')
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
+ENV.each { |k, v| env(k, v) }
 
 every 1.minute do
   rake "job:process_link_view_records"
