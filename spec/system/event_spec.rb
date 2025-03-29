ff11df10c691c291ff91d71290f464ae13d87ef8
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-29 17:27:34 +0900

    fix: Rubocop修正

--- spec/system/event_spec.rb
+++ spec/system/event_spec.rb
@@ -1,15 +1,15 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Events", type: :system do
+RSpec.describe 'Events', type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  describe "未ログイン時" do
-    context "イベント一覧ページにアクセスした時" do
-      it "イベント一覧ページが表示される" do
+  describe '未ログイン時' do
+    context 'イベント一覧ページにアクセスした時' do
+      it 'イベント一覧ページが表示される' do
         visit events_path
-        expect(page).to have_content("イベントだよ")
+        expect(page).to have_content('イベント一覧')
       end
     end
   end
