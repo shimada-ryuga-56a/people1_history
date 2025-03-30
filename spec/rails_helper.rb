246b47976b89e6bccd7035c9928c19bf499d0301
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-30 14:24:12 +0900

    add: sign_inメソッド 使用セットアップ

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -83,6 +83,7 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
 
 Shoulda::Matchers.configure do |config|
