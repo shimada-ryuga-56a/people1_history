d368c1788034ff5d14c32b44ba0d962a072470d6
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-30 15:08:15 +0900

    add: イベント詳細ページに関するテスト記述

--- spec/system/event_spec.rb
+++ spec/system/event_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Events', type: :system do
   let(:user) { create(:user) }
+  let(:tour) { create(:tour, name: 'ツアー1') }
   let(:event1) { create(:event, name: 'イベント1') }
   let(:event2) { create(:event, name: 'イベント2') }
   let(:event3) { create(:event, name: 'イベント3') }
-  let(:event4) { create(:event, name: 'イベント4') }
-  let(:tour) { create(:tour, name: 'ツアー1', event: event4) }
+  let(:event4) { create(:event, name: 'イベント4', category: "tour", tour: tour) }
 
   before do
     driven_by(:rack_test)
@@ -40,6 +40,14 @@ RSpec.describe 'Events', type: :system do
         visit event_path(event1)
         expect(page).to have_content('イベント1')
       end
+
+      it 'ツアーだった場合、ツアー情報を見るためにログインが必要であることを表示する' do
+        event4
+        tour
+        visit event_path(event4)
+        click_link "ツアー詳細"
+        expect(page).to have_content('ログインが必要です')
+      end
     end
   end
 
@@ -67,6 +75,21 @@ RSpec.describe 'Events', type: :system do
         visit events_path
         click_link 'イベント1'
         expect(page).to have_current_path(event_path(event1))
+      end
+    end
+
+    context 'イベント詳細ページにアクセスした時' do
+      it 'イベント詳細ページが表示される' do
+        visit event_path(event1)
+        expect(page).to have_content('イベント1')
+      end
+
+      it 'ツアーだった場合、ツアー情報が表示される' do
+        event4
+        tour
+        visit event_path(event4)
+        click_link "ツアー詳細"
+        expect(page).to have_current_path(tour_path(tour))
       end
     end
   end
